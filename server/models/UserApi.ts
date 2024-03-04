102f6c08e2195cc711f7d65e7d87ba75fe8dcef4
jrivals <jerome.rivals@gmail.com>
2024-03-04 16:16:11 +0100

    Cleanup

--- server/models/UserApi.ts
+++ server/models/UserApi.ts
@@ -1,29 +1,3 @@
-import { UserRole } from '../../shared/types/UserRole';
-import { Department } from '../../shared/types/Department';
-import { WeeklyHours } from '../../shared/types/WeeklyHours';
-
-export interface UserInfosApi {
+export type UserApi = {
   id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  department?: Department;
-  role: UserRole;
-  manager: boolean;
-  hourlyRate?: number;
-  weeklyHours: WeeklyHours;
-}
-
-export type UserApi = UserInfosApi & {
-  password: string;
-  firstName: string;
-  createdAt: Date;
-  lastAuthenticatedAt?: Date;
 };
-
-export interface TokenPayload {
-  userId: string;
-}
-
-export const isAdmin = (user: UserApi) =>
-  user.role === 'Admin' || user.role === 'Root';
