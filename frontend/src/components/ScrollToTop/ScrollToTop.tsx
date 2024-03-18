ea68ea3b97c7a71e2e83398d507f11d90de32e11
jrivals <jerome.rivals@gmail.com>
2024-03-18 17:40:46 +0100

    Cleanup

--- frontend/src/components/ScrollToTop/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop/ScrollToTop.tsx
@@ -4,8 +4,6 @@ import { useLocation } from 'react-router-dom';
 export default function ScrollToTop() {
   const { pathname, search } = useLocation();
 
-  console.log('ScrollToTop', pathname, search);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname, search]);
