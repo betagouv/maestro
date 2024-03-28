596a390443b47d46cdddfdad016c8ac5b0fc461d
jrivals <jerome.rivals@gmail.com>
2024-03-28 09:24:25 +0100

    Ordering prescriptions

--- shared/schema/Prescription/PrescriptionsByMatrix.ts
+++ shared/schema/Prescription/PrescriptionsByMatrix.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { RegionList } from '../Region';
-import { SampleStage } from '../Sample/SampleStage';
+import { SampleStage, SampleStageList } from '../Sample/SampleStage';
 import { Prescription } from './Prescription';
 
 export const PrescriptionByMatrix = z.object({
@@ -16,23 +16,31 @@ export const genPrescriptionByMatrix = (
   prescriptions?: Prescription[],
   includedRegions = RegionList
 ): PrescriptionByMatrix[] =>
-  (prescriptions ?? []).reduce((acc, prescription) => {
-    const index = acc.findIndex(
-      (p) =>
-        p.programmingPlanId === prescription.programmingPlanId &&
-        p.sampleMatrix === prescription.sampleMatrix &&
-        p.sampleStage === prescription.sampleStage
+  (prescriptions ?? [])
+    .reduce((acc, prescription) => {
+      const index = acc.findIndex(
+        (p) =>
+          p.programmingPlanId === prescription.programmingPlanId &&
+          p.sampleMatrix === prescription.sampleMatrix &&
+          p.sampleStage === prescription.sampleStage
+      );
+      if (index === -1) {
+        acc.push({
+          programmingPlanId: prescription.programmingPlanId,
+          sampleMatrix: prescription.sampleMatrix,
+          sampleStage: prescription.sampleStage,
+          regionSampleCounts: new Array(includedRegions.length).fill(0),
+        });
+      }
+      acc[index === -1 ? acc.length - 1 : index].regionSampleCounts[
+        includedRegions.indexOf(prescription.region)
+      ] = prescription.sampleCount;
+      return acc;
+    }, [] as PrescriptionByMatrix[])
+    .sort((a, b) =>
+      `${a.sampleMatrix}-${SampleStageList.indexOf(
+        a.sampleStage
+      )}`.localeCompare(
+        `${b.sampleMatrix}-${SampleStageList.indexOf(b.sampleStage)}`
+      )
     );
-    if (index === -1) {
-      acc.push({
-        programmingPlanId: prescription.programmingPlanId,
-        sampleMatrix: prescription.sampleMatrix,
-        sampleStage: prescription.sampleStage,
-        regionSampleCounts: new Array(includedRegions.length).fill(0),
-      });
-    }
-    acc[index === -1 ? acc.length - 1 : index].regionSampleCounts[
-      includedRegions.indexOf(prescription.region)
-    ] = prescription.sampleCount;
-    return acc;
-  }, [] as PrescriptionByMatrix[]);
