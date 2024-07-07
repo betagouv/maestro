2a2b38556eb3aa9149f1682d7bd7489c2505b0fa
jrivals <jerome.rivals@gmail.com>
2024-07-07 19:43:26 +0200

    Fix prescriptions data

--- database/seeds/dummy/004-prescriptions.ts
+++ database/seeds/dummy/004-prescriptions.ts
@@ -45,7 +45,7 @@ exports.seed = async function () {
     //Choux-fleurs
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A00FR', ['STADE1'], [2,0,13,0,0,0,7,0,6,5,0,0,3,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Endives
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A00NE', ['STADE1'], [0,0,5,0,0,4,10,0,5,0,0,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A00NE', ['STADE1'], [0,0,5,0,0,4,6,0,5,0,0,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Fenouils
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A00SA', ['STADE1'], [4,0,3,0,0,0,0,0,0,5,0,0,4,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Fèves (non écossées)
@@ -54,16 +54,20 @@ exports.seed = async function () {
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A01HG', ['STADE1'], [3,0,0,0,2,0,0,0,0,0,5,0,9,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Jeunes pousses (y compris les espèces du genre Brassica) et similaires
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A00MA', ['STADE1'], [4,0,0,2,3,4,0,5,0,0,3,4,6,3,4,2,0,0], oneOf(DummyLaboratoryIds)),
+    //Fruits à coques
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A014C', ['STADE1'], [0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    //Houblon
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A00YZ', ['STADE3'], [0,0,0,0,0,6,2,0,0,0,0,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Laitues et similaires
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DLB', ['STADE1'], [0,0,0,0,0,0,0,0,0,0,0,0,0,3,4,3,0,4], oneOf(DummyLaboratoryIds)),
     //Légumes secs (graines séchées de légumineuse)
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A012R', ['STADE1'], [6,7,6,9,2,10,12,7,6,15,10,7,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A012R', ['STADE1'], [6,7,6,9,2,4,9,7,6,15,10,3,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Lentilles (sèches)
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A013Q', ['STADE1', 'STADE3'], [9,5,0,5,0,6,0,0,0,6,5,2,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A013Q', ['STADE1', 'STADE3'], [9,5,0,5,0,3,0,0,0,6,5,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Litchis
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A01JV', ['STADE1'], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,6,2], oneOf(DummyLaboratoryIds)),
     //Mâche
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A00KT', ['STADE1'], [0,0,3,0,0,0,3,0,4,4,0,9,3,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A00KT', ['STADE1'], [0,0,3,0,0,0,3,0,4,4,0,5,3,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Mangues
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A01LF', ['STADE1'], [0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,3,4,2], oneOf(DummyLaboratoryIds)),
     //Navets
@@ -71,21 +75,21 @@ exports.seed = async function () {
     //Oignons
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A00HC', ['STADE1'], [6,5,0,6,4,7,6,4,3,3,5,3,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Orge et similaires
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0D9Y', ['STADE1', 'STADE3'], [5,6,6,5,2,8,4,3,5,8,5,5,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0D9Y', ['STADE1', 'STADE3'], [5,6,6,5,2,8,4,3,5,8,5,7,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Patates douces
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A010C', ['STADE1'], [0,0,0,0,0,0,0,0,0,0,2,0,3,5,4,4,3,0], oneOf(DummyLaboratoryIds)),
     //Pêches et similaires
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A01GL', ['STADE1'], [6,0,0,0,5,5,0,0,0,5,10,0,7,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A01GL', ['STADE1'], [6,0,0,0,5,3,0,0,0,5,10,0,7,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Poireaux et similaires
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DEH', ['STADE1'], [6,0,3,5,0,0,4,5,6,4,3,6,0,3,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DEH', ['STADE1'], [6,0,3,5,0,0,4,5,6,4,3,3,0,3,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Poires
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A01DP', ['STADE1'], [6,0,2,2,0,3,0,0,2,4,5,2,5,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A01DP', ['STADE1'], [6,0,2,2,0,3,0,0,2,4,5,7,5,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Riz et similaires
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A001C', ['STADE2'], [0,0,0,0,0,0,0,0,0,0,3,0,8,0,0,6,0,0], oneOf(DummyLaboratoryIds)),
     //Fèves de soja (non écossées)
     ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DFR', ['STADE1', 'STADE3'], [7,8,0,4,0,6,0,2,0,11,12,0,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
     //Graines de tournesol et similaires
-    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DBP', ['STADE1', 'STADE3'], [0,6,0,0,0,4,0,0,0,0,0,7,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
+    ...genPrescriptions(validatedControlProgrammingPlan.id,'A0DBP', ['STADE1', 'STADE3'], [0,6,0,0,0,4,4,0,0,0,0,10,0,0,0,0,0,0], oneOf(DummyLaboratoryIds)),
   ]);
 
   if (!validatedSurveillanceProgrammingPlan) {
