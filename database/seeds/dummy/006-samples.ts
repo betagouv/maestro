0d0bba5134d8d813bbbe38396327b4b52746a373
jrivals <jerome.rivals@gmail.com>
2024-08-09 16:16:27 +0200

    Fix build

--- database/seeds/dummy/006-samples.ts
+++ database/seeds/dummy/006-samples.ts
@@ -30,87 +30,101 @@ exports.seed = async function () {
 
   const samples = [
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A0DVX',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A0DVX',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 2 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A000F',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A000F',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 8 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A00QH',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A00QH',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 3 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A01GG',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A01GG',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 4 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A00HC',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A00HC',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 7 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A0DFB',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A0DFB',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 6 }
     ),
     faker.helpers.multiple<Sample>(
-      () => ({
-        ...genCreatedSample(sampler, validatedControlProgrammingPlan.id),
-        matrix: 'A0DFR',
-        stage: 'STADE1',
-        status: 'Sent',
-        department: oneOf(Regions[sampler.region!].departments),
-        company: oneOf(companies),
-        laboratoryId: oneOf(DummyLaboratoryIds),
-      }),
+      () =>
+        genCreatedSample({
+          sampler,
+          programmingPlanId: validatedControlProgrammingPlan.id,
+          matrix: 'A0DFR',
+          stage: 'STADE1',
+          status: 'Sent',
+          department: oneOf(Regions[sampler.region!].departments),
+          company: oneOf(companies),
+          laboratoryId: oneOf(DummyLaboratoryIds),
+        }),
       { count: 6 }
     ),
   ];
