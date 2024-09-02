a243e4a0592b66e36ae9f7fbac6be16beb8a8ec7
jrivals <jerome.rivals@gmail.com>
2024-09-02 16:44:15 +0200

    Fix tests

--- server/test/globalTeardown.ts
+++ server/test/globalTeardown.ts
@@ -5,10 +5,5 @@ const knexStringcase = require('knex-stringcase');
 
 export default async function teardown() {
   const db = knex(knexStringcase(knexConfig));
-  try {
-    await db.migrate.rollback(undefined, true);
-    console.log('Rolled back.');
-  } finally {
-    await db.destroy();
-  }
+  await db.destroy();
 }
