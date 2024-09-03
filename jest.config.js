78821ab265d65bba477254dadd1bf76f23d9dc18
jrivals <jerome.rivals@gmail.com>
2024-09-03 09:15:57 +0200

    Try to solve the problem on github actions (timeout on tests)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './server',
-  roots: ['../shared'],
+  roots: [],
   setupFiles: [],
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
   globalSetup: '<rootDir>/test/globalSetup.ts',
