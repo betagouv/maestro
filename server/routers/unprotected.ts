6978bde5c6b80d9fdace5450f6556f56a576a52d
jrivals <jerome.rivals@gmail.com>
2024-09-09 15:57:39 +0200

    Try to fix encoding issue

--- server/routers/unprotected.ts
+++ server/routers/unprotected.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { constants } from 'http2';
 import { SignIn } from '../../shared/schema/SignIn';
 import accountController from '../controllers/accountController';
 import { jwtCheck, userCheck } from '../middlewares/checks/authCheck';
@@ -8,16 +7,6 @@ import validator, { body } from '../middlewares/validator';
 const router = express.Router();
 router.use(jwtCheck(false));
 router.use(userCheck(false));
-
-router.get('/test', (req, res) => {
-  console.log('test exécuté');
-
-  res.set('Content-Type', 'application/json; charset=utf-8');
-
-  return res.status(constants.HTTP_STATUS_OK).json({
-    result: 'test exécuté',
-  });
-});
 
 router.post(
   '/accounts/sign-in',
