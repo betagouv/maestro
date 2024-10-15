8e3a6272e6732ec7fa44409ef8fc34978a899565
jrivals <jerome.rivals@gmail.com>
2024-10-15 11:18:48 +0200

    Handle card expansion on mobile devices

--- frontend/src/components/SampleCard/SampleCard.tsx
+++ frontend/src/components/SampleCard/SampleCard.tsx
@@ -3,6 +3,7 @@ import Card from '@codegouvfr/react-dsfr/Card';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import clsx from 'clsx';
 import { format } from 'date-fns';
+import { useState } from 'react';
 import { DepartmentLabels } from 'shared/referential/Department';
 import { MatrixLabels } from 'shared/referential/Matrix/MatrixLabels';
 import { ProgrammingPlanKindLabels } from 'shared/schema/ProgrammingPlan/ProgrammingPlanKind';
@@ -18,6 +19,7 @@ import SampleStatusBadge from 'src/components/SampleStatusBadge/SampleStatusBadg
 import RemoveSample from 'src/components/SampleTable/RemoveSample';
 import { useAuthentication } from 'src/hooks/useAuthentication';
 import { useOnLine } from 'src/hooks/useOnLine';
+import useWindowSize from 'src/hooks/useWindowSize';
 import './SampleCard.scss';
 
 interface Props {
@@ -29,9 +31,40 @@ interface Props {
 const SampleCard = ({ sample, sampleProgrammingPlan }: Props) => {
   const { userInfos, hasPermission } = useAuthentication();
   const { isOnline } = useOnLine();
+  const { isMobile } = useWindowSize();
+
+  const [isExpanded, setIsExpanded] = useState(!isMobile);
+
   return (
     <Card
-      start={<SampleStatusBadge status={sample?.status} />}
+      start={
+        <div className={clsx('d-flex-align-start')}>
+          <div className="flex-grow-1">
+            <SampleStatusBadge status={sample?.status} />
+          </div>
+          {isMobile && (
+            <>
+              {isExpanded ? (
+                <Button
+                  title="Réduire"
+                  iconId="fr-icon-subtract-line"
+                  size="small"
+                  priority="tertiary"
+                  onClick={() => setIsExpanded(false)}
+                />
+              ) : (
+                <Button
+                  title="Voir plus"
+                  iconId="fr-icon-add-line"
+                  size="small"
+                  priority="tertiary"
+                  onClick={() => setIsExpanded(true)}
+                />
+              )}
+            </>
+          )}
+        </div>
+      }
       title={isCreatedPartialSample(sample) ? sample.reference : 'Hors ligne'}
       border
       linkProps={{
@@ -54,66 +87,74 @@ const SampleCard = ({ sample, sampleProgrammingPlan }: Props) => {
               )}
             </span>
           </span>
-          <span className="icon-text">
-            <span
-              className={cx('fr-icon-calendar-event-line', 'fr-icon--sm')}
-            ></span>
-            {format(sample.sampledAt, 'dd/MM/yyyy')}
-          </span>
-          <span className="icon-text">
-            <span className={cx('fr-icon-road-map-line', 'fr-icon--sm')}></span>
-            {DepartmentLabels[sample.department]} ({sample.department})
-          </span>
-          {sampleProgrammingPlan && (
-            <span className="icon-text">
-              <span
-                className={cx('fr-icon-microscope-line', 'fr-icon--sm')}
-              ></span>
-              {ProgrammingPlanKindLabels[sampleProgrammingPlan?.kind]}
-            </span>
+          {isExpanded && (
+            <>
+              <span className="icon-text">
+                <span
+                  className={cx('fr-icon-calendar-event-line', 'fr-icon--sm')}
+                ></span>
+                {format(sample.sampledAt, 'dd/MM/yyyy')}
+              </span>
+              <span className="icon-text">
+                <span
+                  className={cx('fr-icon-road-map-line', 'fr-icon--sm')}
+                ></span>
+                {DepartmentLabels[sample.department]} ({sample.department})
+              </span>
+              {sampleProgrammingPlan && (
+                <span className="icon-text">
+                  <span
+                    className={cx('fr-icon-microscope-line', 'fr-icon--sm')}
+                  ></span>
+                  {ProgrammingPlanKindLabels[sampleProgrammingPlan?.kind]}
+                </span>
+              )}
+              {sample.matrix && (
+                <span className="icon-text">
+                  <span
+                    className={cx('fr-icon-restaurant-line', 'fr-icon--sm')}
+                  ></span>
+                  {MatrixLabels[sample.matrix]}
+                </span>
+              )}
+              <span className="icon-text">
+                <span className={cx('fr-icon-user-line', 'fr-icon--sm')}></span>
+                {isCreatedPartialSample(sample)
+                  ? `${sample.sampler.firstName} ${sample.sampler.lastName}`
+                  : `${userInfos?.firstName} ${userInfos?.lastName}`}
+              </span>
+            </>
           )}
-          {sample.matrix && (
-            <span className="icon-text">
-              <span
-                className={cx('fr-icon-restaurant-line', 'fr-icon--sm')}
-              ></span>
-              {MatrixLabels[sample.matrix]}
-            </span>
-          )}
-          <span className="icon-text">
-            <span className={cx('fr-icon-user-line', 'fr-icon--sm')}></span>
-            {isCreatedPartialSample(sample)
-              ? `${sample.sampler.firstName} ${sample.sampler.lastName}`
-              : `${userInfos?.firstName} ${userInfos?.lastName}`}
-          </span>
         </span>
       }
       size="small"
       titleAs="h6"
       end={
-        <div className={clsx('d-flex-align-center')}>
-          <Button
-            size="small"
-            className={clsx('fr-mr-2w')}
-            linkProps={{
-              to: `/prelevements/${sample.id}`,
-            }}
-            priority={
-              [...DraftStatusList, 'Submitted'].includes(sample.status)
-                ? 'primary'
-                : 'secondary'
-            }
-          >
-            {[...DraftStatusList, 'Submitted'].includes(sample.status)
-              ? 'A compléter'
-              : 'Consulter'}
-          </Button>
-          {isOnline &&
-            hasPermission('deleteSample') &&
-            DraftStatusList.includes(sample.status) && (
-              <RemoveSample sample={sample} />
-            )}
-        </div>
+        isExpanded && (
+          <div className={clsx('d-flex-align-center')}>
+            <Button
+              size="small"
+              className={clsx('fr-mr-2w')}
+              linkProps={{
+                to: `/prelevements/${sample.id}`,
+              }}
+              priority={
+                [...DraftStatusList, 'Submitted'].includes(sample.status)
+                  ? 'primary'
+                  : 'secondary'
+              }
+            >
+              {[...DraftStatusList, 'Submitted'].includes(sample.status)
+                ? 'A compléter'
+                : 'Consulter'}
+            </Button>
+            {isOnline &&
+              hasPermission('deleteSample') &&
+              DraftStatusList.includes(sample.status) && (
+                <RemoveSample sample={sample} />
+              )}
+          </div>
+        )
       }
     />
   );
