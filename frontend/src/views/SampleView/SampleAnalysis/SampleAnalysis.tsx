c48ccba6f65e261450d1c471b67a1f3a9f29b4b9
jrivals <jerome.rivals@gmail.com>
2024-10-16 14:23:25 +0200

    Rework UX transition between sample admissibility and analysis

--- frontend/src/views/SampleView/SampleAnalysis/SampleAnalysis.tsx
+++ frontend/src/views/SampleView/SampleAnalysis/SampleAnalysis.tsx
@@ -1,10 +1,15 @@
 import Alert from '@codegouvfr/react-dsfr/Alert';
+import Button from '@codegouvfr/react-dsfr/Button';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import clsx from 'clsx';
+import { useState } from 'react';
 import { Sample } from 'shared/schema/Sample/Sample';
 import { CompletedStatusList } from 'shared/schema/Sample/SampleStatus';
 import SampleStatusBadge from 'src/components/SampleStatusBadge/SampleStatusBadge';
-import { useUpdateAnalysisMutation } from 'src/services/analysis.service';
+import {
+  useGetSampleAnalysisQuery,
+  useUpdateAnalysisMutation,
+} from 'src/services/analysis.service';
 import { useGetLaboratoryQuery } from 'src/services/laboratory.service';
 import { useUpdateSampleMutation } from 'src/services/sample.service';
 import SampleAdmissibility from 'src/views/SampleView/SampleAnalysis/SampleAdmissibility/SampleAdmissibility';
@@ -25,6 +30,10 @@ const SampleAnalysis = ({ sample }: Props) => {
   const { data: laboratory } = useGetLaboratoryQuery(sample.laboratoryId, {
     skip: !isSendingSuccess,
   });
+  const { data: analysis, isFetching: isAnalysisFetching } =
+    useGetSampleAnalysisQuery(sample.id);
+
+  const [continueToAnalysis, setContinueToAnalysis] = useState(false);
 
   return (
     <div>
@@ -68,27 +77,44 @@ const SampleAnalysis = ({ sample }: Props) => {
       </div>
       <SampleAdmissibility sample={sample} />
 
-      {['Analysis', ...CompletedStatusList].includes(sample.status) && (
-        <div
-          className={clsx(
-            cx(
-              'fr-callout',
-              CompletedStatusList.includes(sample.status)
-                ? 'fr-callout--green-emeraude'
-                : 'fr-callout--pink-tuile'
-            ),
-            'sample-callout',
-            'analysis-container',
-            'fr-mt-5w'
+      {!isAnalysisFetching && (
+        <>
+          {sample.status === 'Analysis' && !analysis && !continueToAnalysis ? (
+            <Button
+              iconId="fr-icon-arrow-down-line"
+              iconPosition="right"
+              className="fr-m-0"
+              onClick={() => setContinueToAnalysis(true)}
+            >
+              Saisir le résultat
+            </Button>
+          ) : (
+            <>
+              {['Analysis', ...CompletedStatusList].includes(sample.status) && (
+                <div
+                  className={clsx(
+                    cx(
+                      'fr-callout',
+                      CompletedStatusList.includes(sample.status)
+                        ? 'fr-callout--green-emeraude'
+                        : 'fr-callout--pink-tuile'
+                    ),
+                    'sample-callout',
+                    'analysis-container',
+                    'fr-mt-5w'
+                  )}
+                >
+                  {sample.status === 'Analysis' && (
+                    <SampleDraftAnalysis sample={sample} />
+                  )}
+                  {CompletedStatusList.includes(sample.status) && (
+                    <SampleAnalysisOverview sample={sample} />
+                  )}
+                </div>
+              )}
+            </>
           )}
-        >
-          {sample.status === 'Analysis' && (
-            <SampleDraftAnalysis sample={sample} />
-          )}
-          {CompletedStatusList.includes(sample.status) && (
-            <SampleAnalysisOverview sample={sample} />
-          )}
-        </div>
+        </>
       )}
     </div>
   );
