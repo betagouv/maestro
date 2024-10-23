efa51173bb84fed91f0935143f908dcbe875f8ac
jrivals <jerome.rivals@gmail.com>
2024-10-23 14:49:22 +0200

    Fix tests

--- server/routers/programmingPlan.router.ts
+++ server/routers/programmingPlan.router.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import { FindProgrammingPlanOptions } from '../../shared/schema/ProgrammingPlan/FindProgrammingPlanOptions';
 import programmingPlanController from '../controllers/programmingPlanController';
 import { permissionsCheck } from '../middlewares/checks/authCheck';
-import { programmingPlanCheck } from '../middlewares/checks/programmingPlanCheck';
-import validator, { query, uuidParam } from '../middlewares/validator';
+import validator, { query } from '../middlewares/validator';
 
 const router = express.Router();
 
@@ -12,13 +11,6 @@ router.get(
   validator.validate(query(FindProgrammingPlanOptions)),
   permissionsCheck(['readProgrammingPlans']),
   programmingPlanController.findProgrammingPlans
-);
-router.get(
-  '/:programmingPlanId',
-  validator.validate(uuidParam('programmingPlanId')),
-  permissionsCheck(['readProgrammingPlans']),
-  programmingPlanCheck(),
-  programmingPlanController.getProgrammingPlan
 );
 
 export default router;
