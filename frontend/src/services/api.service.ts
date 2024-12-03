78ee416697dc064ca7fd30e839f01a6e3db33297
vmaubert <github@vcmb.dev>
2024-12-03 10:51:47 +0100

    remplace craco par vite

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,6 +1,6 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 import { withAuthHeader } from 'src/services/auth-headers';
 import config from '../utils/config';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
