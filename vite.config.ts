2b1c80496e594a2b96a30f7251e5fea8fa97c207
vmaubert <github@vcmb.dev>
2024-12-05 17:26:49 +0100

    simplifie le path pour vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     setupFiles: [],
-    include: ['shared/**/**/*.test.unit.ts', 'server/**/**/*.test.unit.ts']
+    include: ['shared/**/*.test.unit.ts', 'server/**/*.test.unit.ts']
  }
 })
