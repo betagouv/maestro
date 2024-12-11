fb512744255f600e297a6aee0a6cf9d34fa0da9d
jrivals <jerome.rivals@gmail.com>
2024-12-11 15:24:26 +0100

    Implements proConnect (WIP)

--- shared/schema/Auth/AuthRedirectUrl.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const AuthRedirectUrl = z.object({
-  url: z.string().url(),
-  nonce: z.string(),
-  state: z.string()
-});
-
-export type AuthRedirectUrl = z.infer<typeof AuthRedirectUrl>;
