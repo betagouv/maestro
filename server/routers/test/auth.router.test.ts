9fac886b3b50ab112f7272553d52bedffa1f2577
jrivals <jerome.rivals@gmail.com>
2024-12-16 12:06:33 +0100

    Fix test

--- server/routers/test/auth.router.test.ts
+++ server/routers/test/auth.router.test.ts
@@ -1,16 +1,15 @@
-import { fakerFR } from '@faker-js/faker';
 import bcrypt from 'bcryptjs';
 import { constants } from 'http2';
-import randomstring from 'randomstring';
 import request from 'supertest';
-import { genUser, genValidPassword } from '../../../shared/test/userFixtures';
+import { genUser } from '../../../shared/test/userFixtures';
 import { Users } from '../../repositories/userRepository';
 import { createServer } from '../../server';
 
-import { describe, test, expect, beforeAll, afterAll } from 'vitest';
-const { app } = createServer();
+import { afterAll, beforeAll, describe, expect, test } from 'vitest';
 
 describe('Auth routes', () => {
+  const { app } = createServer();
+
   const user = genUser();
 
   beforeAll(async () => {
@@ -25,62 +24,16 @@ describe('Auth routes', () => {
     await Users().delete().where('email', user.email);
   });
 
-  describe('POST /auth/sign-in', () => {
-    const testRoute = '/api/auth/sign-in';
+  describe('GET /auth/redirect-url', () => {
+    const testRoute = '/api/auth/redirect-url';
 
-    test('should receive valid email and password', async () => {
-      await request(app)
-        .post(testRoute)
-        .send({
-          email: randomstring.generate(),
-          password: genValidPassword()
-        })
-        .expect(constants.HTTP_STATUS_BAD_REQUEST);
+    test('should return a redirect url', async () => {
+      const response = await request(app).get(testRoute);
 
-      await request(app)
-        .post(testRoute)
-        .send({
-          email: fakerFR.internet.email(),
-          password: '   '
-        })
-        .expect(constants.HTTP_STATUS_BAD_REQUEST);
-    });
-
-    test('should fail if the user is missing', async () => {
-      await request(app)
-        .post(testRoute)
-        .send({
-          email: fakerFR.internet.email(),
-          password: genValidPassword()
-        })
-        .expect(constants.HTTP_STATUS_UNAUTHORIZED);
-    });
-
-    test('should fail if the password is wrong', async () => {
-      await request(app)
-        .post(testRoute)
-        .send({
-          email: user.email,
-          password: genValidPassword()
-        })
-        .expect(constants.HTTP_STATUS_UNAUTHORIZED);
-    });
-
-    test('should succeed if the user is found and the password is correct', async () => {
-      const res = await request(app)
-        .post(testRoute)
-        .send({
-          email: user.email,
-          password: user.password
-        })
-        .expect(constants.HTTP_STATUS_OK);
-
-      expect(res.body).toMatchObject(
-        expect.objectContaining({
-          userId: user.id,
-          accessToken: expect.any(String)
-        })
-      );
+      expect(response.status).toBe(constants.HTTP_STATUS_OK);
+      expect(response.body).toHaveProperty('url');
+      expect(response.body).toHaveProperty('state');
+      expect(response.body).toHaveProperty('nonce');
     });
   });
 });
