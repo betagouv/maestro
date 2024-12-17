9b7cea0003ebc1ab5e0da3249049f058ab37fe13
jrivals <jerome.rivals@gmail.com>
2024-12-17 10:17:00 +0100

    Change dummy emails

--- database/seeds/dummy/003-programming-plans.ts
+++ database/seeds/dummy/003-programming-plans.ts
@@ -11,7 +11,7 @@ exports.seed = async function (knex: Knex) {
   setKnexInstance(knex);
 
   const user = await Users()
-    .where('email', 'coordinateur.national@yopmail.fr')
+    .where('email', 'coordinateur.national@maestro.beta.gouv.fr.fr')
     .first();
 
   if (!user) {
