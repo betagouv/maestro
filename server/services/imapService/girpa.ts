c54b76016009d48491c3ce3722cd65a4b4b73325
vmaubert <github@vcmb.dev>
2024-12-18 16:58:43 +0100

    ajoute une lib pour parser du xml

--- server/services/imapService/girpa.ts
+++ server/services/imapService/girpa.ts
@@ -1,9 +1,56 @@
 import { ExportDataFromEmail, IsSender, LaboratoryConf } from './index';
+import { XMLParser } from 'fast-xml-parser';
+import { z } from 'zod';
 
 
 const isSender: IsSender = (_emailSender) => true
 
-const exportDataFromEmail: ExportDataFromEmail = (email) => email
+
+const frenchNumberStringValidator = z
+    .string()
+    .transform((val) => Number(`${val}`.replace(',', '.')))
+    .pipe(z.number())
+
+const exportDataFromEmail: ExportDataFromEmail = (email) => {
+
+  const xmlFile = email.attachments.find(({contentType}) => contentType === 'text/xml')
+
+  if (xmlFile !== undefined) {
+    const parser = new XMLParser();
+    const validator = z.object({
+      Rapport: z.object({
+        Echantillon: z.object({
+          'Code_échantillon': z.string(),
+          Commentaire: z.string(),
+          Analyse: z.array(
+            z.object({
+              'Résultat': frenchNumberStringValidator,
+              'Unité': z.string(),
+              'Limite_de_quantification': frenchNumberStringValidator,
+              'LMR': z.union([z.number(), frenchNumberStringValidator]),
+              'Substance_active_CAS': z.string(),
+              'Substance_active_anglais': z.string()
+            })
+          )
+        })
+      })
+    })
+    const obj = parser.parse(xmlFile.content)
+
+    const result = validator.safeParse(obj)
+
+    if (result.success) {
+      console.log(result.data.Rapport.Echantillon.Analyse.filter((a) => a.Résultat > a.LMR || a.Résultat > (a.Limite_de_quantification/3)))
+      return { sampleReference: result.data.Rapport.Echantillon['Code_échantillon']}
+    }else {
+      console.log('Erreur: ', result.error)
+    }
+
+  }
+
+
+  return null
+}
 
 
 export const girpaConf: LaboratoryConf = {
