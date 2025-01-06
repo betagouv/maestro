f79fb5efa8d0df951f79c214b94429f9190c681c
jrivals <jerome.rivals@gmail.com>
2025-01-06 14:58:10 +0100

    Fix import of beforeEach from vitest

--- frontend/src/views/SampleView/test/SampleView.test.tsx
+++ frontend/src/views/SampleView/test/SampleView.test.tsx
@@ -7,18 +7,17 @@ import { genProgrammingPlan } from 'shared/test/programmingPlanFixtures';
 import {
   genCreatedPartialSample,
   genCreatedSampleData,
-  genSampleContextData,
+  genSampleContextData
 } from 'shared/test/sampleFixtures';
 import { genAuthUser, genUser } from 'shared/test/userFixtures';
 import { applicationMiddleware, applicationReducer } from 'src/store/store';
 import SampleView from 'src/views/SampleView/SampleView';
 import {
   getRequestCalls,
-  mockRequests,
+  mockRequests
 } from '../../../../test/requestTestUtils';
 
-import { describe, test, expect, vi } from 'vitest';
-
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 
 vi.mock(import('react-router-dom'), async (importOriginal) => {
   const original = await importOriginal();
@@ -32,29 +31,29 @@ let store: Store;
 const authUser = genAuthUser();
 const sampler = genUser({
   roles: ['Sampler'],
-  id: authUser.userId,
+  id: authUser.userId
 });
 const userRequest = {
   pathname: `/api/users/${sampler.id}/infos`,
-  response: { body: JSON.stringify(sampler) },
+  response: { body: JSON.stringify(sampler) }
 };
 const programmingPlan1 = genProgrammingPlan();
 const programmingPlan2 = genProgrammingPlan();
 const programmingPlanRequest = {
   pathname: `/api/programming-plans?status=Validated`,
   response: {
-    body: JSON.stringify([programmingPlan1, programmingPlan2]),
-  },
+    body: JSON.stringify([programmingPlan1, programmingPlan2])
+  }
 };
 const prescriptions = [
   genPrescription({
     programmingPlanId: programmingPlan1.id,
-    context: 'Control',
-  }),
+    context: 'Control'
+  })
 ];
 const prescriptionsRequest = {
   pathname: `/api/programming-plans/${programmingPlan1.id}/prescriptions?`,
-  response: { body: JSON.stringify(prescriptions) },
+  response: { body: JSON.stringify(prescriptions) }
 };
 
 describe('SampleView', () => {
@@ -64,8 +63,8 @@ describe('SampleView', () => {
       reducer: applicationReducer,
       middleware: applicationMiddleware,
       preloadedState: {
-        auth: { authUser },
-      },
+        auth: { authUser }
+      }
     });
   });
 
@@ -91,24 +90,22 @@ describe('SampleView', () => {
   test('should render the second step for a draft sample', async () => {
     const createdSample = {
       ...genSampleContextData({
-        programmingPlanId: programmingPlan1.id,
+        programmingPlanId: programmingPlan1.id
       }),
       ...genCreatedSampleData({
-        sampler,
+        sampler
       }),
-      status: 'DraftMatrix',
+      status: 'DraftMatrix'
     };
     mockRequests([
       userRequest,
       prescriptionsRequest,
       {
         pathname: `/api/samples/${createdSample.id}`,
-        response: { body: JSON.stringify(createdSample) },
-      },
+        response: { body: JSON.stringify(createdSample) }
+      }
     ]);
-   vi
-      .mocked(useParams)
-      .mockReturnValue({ sampleId: createdSample.id });
+    vi.mocked(useParams).mockReturnValue({ sampleId: createdSample.id });
 
     render(
       <Provider store={store}>
@@ -135,18 +132,16 @@ describe('SampleView', () => {
   test('should render the third step for a sample with status DraftItems', async () => {
     const draftSample = {
       ...genCreatedPartialSample(),
-      status: 'DraftItems',
+      status: 'DraftItems'
     };
     mockRequests([
       userRequest,
       {
         pathname: `/api/samples/${draftSample.id}`,
-        response: { body: JSON.stringify(draftSample) },
-      },
+        response: { body: JSON.stringify(draftSample) }
+      }
     ]);
-   vi
-      .mocked(useParams)
-      .mockReturnValue({ sampleId: draftSample.id });
+    vi.mocked(useParams).mockReturnValue({ sampleId: draftSample.id });
 
     render(
       <Provider store={store}>
@@ -171,19 +166,17 @@ describe('SampleView', () => {
   test('should render the fourth step for a sample with status Submitted', async () => {
     const draftSample = {
       ...genCreatedPartialSample(),
-      status: 'Submitted',
+      status: 'Submitted'
     };
     mockRequests([
       userRequest,
       programmingPlanRequest,
       {
         pathname: `/api/samples/${draftSample.id}`,
-        response: { body: JSON.stringify(draftSample) },
-      },
+        response: { body: JSON.stringify(draftSample) }
+      }
     ]);
-   vi
-      .mocked(useParams)
-      .mockReturnValue({ sampleId: draftSample.id });
+    vi.mocked(useParams).mockReturnValue({ sampleId: draftSample.id });
 
     render(
       <Provider store={store}>
