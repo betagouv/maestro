c73e54c7e2cc4d9b0265ff4ad03b636c163b0a5b
vmaubert <github@vcmb.dev>
2025-01-07 12:00:38 +0100

    add logs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,23 +3,31 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './i18n';
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register';
 
 // Vérifie qu'il n'y a pas une nouvelle version du site toutes les 5min
-const intervalMS =  30 * 1000
-registerSW({immediate: true,
+const intervalMS = 30 * 1000;
+
+registerSW({
+  immediate: true,
   onRegisteredSW(_s, r) {
-  console.log('try to register sw')
-    if( r !== undefined ) {
-      console.log('subscribe update')
-      r.update()
+    console.log('try to register sw');
+    if (r !== undefined) {
+      console.log('subscribe update');
       setInterval(() => {
-        console.log('update')
-        r.update()
-      }, intervalMS)
+        console.log('update');
+        r.update();
+      }, intervalMS);
     }
+  },
+  onRegisterError(error) {
+    console.error('error', error)
+  },
+  onNeedRefresh() {
+    console.log('needRefresh')
   }
-})
+
+});
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
