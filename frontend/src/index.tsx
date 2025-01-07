96097d4ecdfba531d04e7144fd7982687a924069
vmaubert <github@vcmb.dev>
2025-01-07 16:37:55 +0100

    skipWaiting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,31 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './i18n';
-import { registerSW } from 'virtual:pwa-register';
+import { registerSW } from 'virtual:pwa-register'
 
 // Vérifie qu'il n'y a pas une nouvelle version du site toutes les 5min
-const intervalMS = 30 * 1000;
-
-registerSW({
-  immediate: true,
+const intervalMS =  30 * 1000
+registerSW({immediate: true,
   onRegisteredSW(_s, r) {
-    console.log('try to register sw');
-    if (r !== undefined) {
-      console.log('subscribe update');
+    if( r !== undefined ) {
       setInterval(() => {
-        console.log('update');
-        r.update();
-      }, intervalMS);
+        r.update()
+      }, intervalMS)
     }
-  },
-  onRegisterError(error) {
-    console.error('error', error)
-  },
-  onNeedRefresh() {
-    console.log('needRefresh')
   }
-
-});
+})
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
