009073c55ae0c7648ee6402e6eefac3d60ae60b4
vmaubert <github@vcmb.dev>
2025-01-08 10:44:17 +0100

    fix warning

--- server/repositories/userRepository.test.ts
+++ server/repositories/userRepository.test.ts
@@ -9,8 +9,8 @@ test("impossible d'avoir 2 utilisateurs avec le même email", async () => {
   await kysely.insertInto('users').values(genUser({email})).execute()
   await kysely.insertInto('users').values(genUser({ email: 'anotheremail@email.fr'})).execute()
 
-  expect(async () =>
-  await kysely.insertInto('users').values(genUser({
+  await expect(async () =>
+   kysely.insertInto('users').values(genUser({
     email,
   })).execute()).rejects.toThrowErrorMatchingInlineSnapshot(`[error: duplicate key value violates unique constraint "users_email_index"]`)
 })
