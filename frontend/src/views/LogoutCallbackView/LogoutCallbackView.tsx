ee8c756084155e9dfd59aaed76f9c277a9dd0324
vmaubert <github@vcmb.dev>
2025-01-22 16:21:24 +0100

    better way

--- frontend/src/views/LogoutCallbackView/LogoutCallbackView.tsx
+++ frontend/src/views/LogoutCallbackView/LogoutCallbackView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from 'src/hooks/useStore';
 import { appLogout } from 'src/store/store';
@@ -8,15 +8,14 @@ export const LogoutCallbackView = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const isUserUnknown = useRef<boolean>(false)
+  const isUserUnknown : boolean = sessionStorage.getItem(SESSION_STORAGE_UNKNOWN_USER) !== null
 
   useEffect(() => {
     (async () => {
       await appLogout()(dispatch);
       if( sessionStorage.getItem(SESSION_STORAGE_UNKNOWN_USER) !== null  ){
         sessionStorage.removeItem(SESSION_STORAGE_UNKNOWN_USER)
-        isUserUnknown.current = true
-      }else if( !isUserUnknown.current ){
+      }else if( !isUserUnknown ){
         navigate('/')
       }
     })();
