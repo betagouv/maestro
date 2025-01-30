d47c0b96983fdd233924df2bf1eae7c1289de012
vmaubert <github@vcmb.dev>
2025-01-30 14:51:11 +0100

    récupère tous les index

--- server/scripts/updateSSD2Referential.ts
+++ server/scripts/updateSSD2Referential.ts
@@ -1,7 +1,63 @@
+import ExcelJS from 'exceljs';
+import path from 'path';
+
 const updateSSD2Referential = async () => {
 
   console.log('Updating SSD2…')
 
+
+  let workbook = new ExcelJS.Workbook();
+  const filePath = path.join(import.meta.dirname,  "PARAM.xlsx")
+  workbook = await workbook.xlsx.readFile(filePath, {
+    ignoreNodes: [
+      "sheetPr",
+      "dimension",
+      "sheetViews",
+      "sheetFormatPr",
+      "cols",
+      // "sheetData",
+      "autoFilter",
+      "mergeCells",
+      "rowBreaks",
+      "hyperlinks",
+      "pageMargins",
+      "dataValidations",
+      "pageSetup",
+      "headerFooter",
+      "printOptions",
+      "picture",
+      "drawing",
+      "sheetProtection",
+      "tableParts",
+      "conditionalFormatting",
+      "extLst",
+    ]
+  });
+
+  const worksheet = workbook.getWorksheet('term')
+
+  if (worksheet === undefined) {
+    throw new Error('impossible de trouver une page term')
+  }
+
+  const columnsIndex: Record<string, number>= {}
+  const firstLine = worksheet.getRow(1)
+  let pestParamReportableIndex
+  firstLine.eachCell((cell, index) => {
+    if( typeof cell.value === 'string') {
+      columnsIndex[cell.value] = index
+    }
+  })
+  console.log('index', columnsIndex)
+
+  worksheet.eachRow(function(row, rowNumber) {
+    if (rowNumber === 1) {
+
+      console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
+    }
+  });
+  console.log(worksheet.getCell('A274').value);
+  console.log(worksheet.rowCount)
 
 
 
