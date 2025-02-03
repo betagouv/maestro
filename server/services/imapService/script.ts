2075ff6267ee9a64add1e167eff4ae2c29fd0317
vmaubert <github@vcmb.dev>
2025-02-03 15:28:51 +0100

    fix(emails): corrige l'accès à la boite email

--- server/services/imapService/script.ts
+++ server/services/imapService/script.ts
@@ -1,10 +1,17 @@
 import { initKysely } from '../../repositories/kysely.js';
 import config from '../../utils/config.js';
 import { checkEmails } from './index.js';
+import { mattermostService } from '../mattermostService';
 
 const launchExtractEmail = async () => {
   initKysely(config.databaseUrl);
-  await checkEmails();
+  try{
+
+    await checkEmails();
+  }catch (e){
+      console.log('error', e)
+    await mattermostService.send('Attention erreur pendant la lecture des emails.')
+  }
 };
 
 export default launchExtractEmail()
