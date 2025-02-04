9768019df05edfc8e9dbcff1d9fbea2fb2fd46b3
jrivals <jerome.rivals@gmail.com>
2025-02-04 16:28:41 +0100

    feat(header) : Lien vers la page aides du site vitrine

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,7 +1,8 @@
 const config = {
   apiEndpoint: process.env.REACT_APP_API_URL,
   satelliteStyle: process.env.REACT_APP_SATELLITE_STYLE,
-  supportEmail: process.env.REACT_APP_SUPPORT_EMAIL
+  supportEmail: process.env.REACT_APP_SUPPORT_EMAIL,
+  websiteUrl: process.env.REACT_APP_WEBSITE_URL
 };
 
 export default config;
