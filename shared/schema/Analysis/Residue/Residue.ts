7332c4e9dc6f5e6fbd62b9ca4dc07e4b7e74785e
jrivals <jerome.rivals@gmail.com>
2025-02-04 10:01:55 +0100

    feat(residue) : Ajout d'un champ pour expliquer la conformité “Autre” d'un résidu

--- shared/schema/Analysis/Residue/Residue.ts
+++ shared/schema/Analysis/Residue/Residue.ts
@@ -8,7 +8,7 @@ import { ResidueCompliance } from './ResidueCompliance';
 import { ResidueKind } from './ResidueKind';
 import { ResultKind } from './ResultKind';
 
-export const Residue = z.object({
+const ResidueBase = z.object({
   analysisId: z.string().uuid(),
   residueNumber: z.number().int().positive(),
   analysisMethod: AnalysisMethod,
@@ -32,8 +32,21 @@ export const Residue = z.object({
   analytes: z.array(Analyte).nullish()
 });
 
-export const PartialResidue = Residue.partial().merge(
-  Residue.pick({
+export const Residue = ResidueBase.refine(
+  (data) => {
+    if (data.compliance === 'Other') {
+      return data.otherCompliance;
+    }
+    return true;
+  },
+  {
+    message: 'Veuillez préciser la conformité la conformité “Autre”.',
+    path: ['otherCompliance']
+  }
+);
+
+export const PartialResidue = ResidueBase.partial().merge(
+  ResidueBase.pick({
     analysisId: true,
     residueNumber: true
   }).merge(
