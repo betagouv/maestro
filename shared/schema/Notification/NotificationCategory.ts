269fafa58c9a957b708bd33fe44316b8890eeee0
jrivals <jerome.rivals@gmail.com>
2025-02-20 09:03:38 +0100

    Fix mattermost notifications

--- shared/schema/Notification/NotificationCategory.ts
+++ shared/schema/Notification/NotificationCategory.ts
@@ -22,11 +22,15 @@ export const NotificationCategoryMessages: Partial<
   Record<NotificationCategory, string>
 > = {
   ProgrammingPlanSubmitted: `
-Maestro vient d’être mis à jour !
-Une proposition de programmation pour la prochaine campagne de surveillance / contrôle officielle a été déposée sur Maestro par la coordination nationale. 
+Maestro vient d’être mis à jour !  
+
+Une proposition de programmation pour la prochaine campagne de surveillance / contrôle officielle a été déposée sur Maestro par la coordination nationale.   
+
 Merci de prendre connaissance de ces nouveaux éléments et y réagir le cas échéant.`,
   ProgrammingPlanValidated: `
-L’étape de programmation a été clôturée par la coordination nationale. 
-En tant que coordinateur régional, vous pouvez dorénavant vous connecter à Maestro sur l’espace "programmation" afin d’attribuer le/les laboratoires responsables des analyses officielles en lien avec les matrices programmées pour la prochaine campagne du dispositif PSPC dans votre région.
+L’étape de programmation a été clôturée par la coordination nationale.  
+
+En tant que coordinateur régional, vous pouvez dorénavant vous connecter à Maestro sur l’espace "programmation" afin d’attribuer le/les laboratoires responsables des analyses officielles en lien avec les matrices programmées pour la prochaine campagne du dispositif PSPC dans votre région.  
+
 Une fois le/les laboratoires attribués, la campagne sera officiellement lancée et les inspecteurs/préleveurs de vos régions pourront initier leurs prélèvements.`
 };
