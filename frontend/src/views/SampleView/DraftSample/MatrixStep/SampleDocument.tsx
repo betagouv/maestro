3a3412b493f0ebda64e666cdc7efab41c0d2016d
jrivals <jerome.rivals@gmail.com>
2025-02-27 15:45:18 +0100

    Affichage responsive des pièces jointes

--- frontend/src/views/SampleView/DraftSample/MatrixStep/SampleDocument.tsx
+++ frontend/src/views/SampleView/DraftSample/MatrixStep/SampleDocument.tsx
@@ -1,6 +1,7 @@
 import Button from '@codegouvfr/react-dsfr/Button';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import Input from '@codegouvfr/react-dsfr/Input';
+import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 import {
   useGetDocumentDownloadSignedUrlQuery,
@@ -8,7 +9,6 @@ import {
   useUpdateDocumentMutation
 } from '../../../../services/document.service';
 import { cropFileName } from '../../../../utils/stringUtils';
-
 interface Props {
   documentId: string;
   onRemove: (documentId: string) => Promise<void>;
@@ -27,48 +27,73 @@ const SampleDocument = ({ documentId, onRemove }: Props) => {
   }
 
   return (
-    <div className="d-flex-align-center">
-      <div className="d-flex-align-start flex-grow-1">
-        <img src={documentUrl} alt={document.filename} width={200} />
-        <div className={cx('fr-ml-3w')}>
-          <Link
-            onClick={async (e) => {
-              e.preventDefault();
-              window.open(documentUrl);
-            }}
-            to="#"
-            className={cx('fr-link')}
-          >
-            {cropFileName(document.filename, 25)}
-            <span
-              className={cx('fr-icon-eye-line', 'fr-ml-1w', 'fr-icon--sm')}
-            ></span>
-          </Link>
-          <Input
-            label=""
-            textArea
-            nativeTextAreaProps={{
-              placeholder: 'Légende',
-              defaultValue: document.legend || '',
-              onChange: (e) => {
-                e.preventDefault();
-                updateDocument({ documentId, legend: e.target.value });
-              },
-              rows: 1
-            }}
-            className={cx('fr-mt-2w')}
-          />
-        </div>
+    <div className={cx('fr-grid-row', 'fr-grid-row--gutters')}>
+      <div className={cx('fr-col-12', 'fr-col-sm-3')}>
+        <img
+          src={documentUrl}
+          alt={document.filename}
+          width="100%"
+          style={{ maxWidth: '200px' }}
+        />
       </div>
-      <Button
-        title="Supprimer"
-        onClick={async (e) => {
-          e.preventDefault();
-          await onRemove(documentId);
-        }}
-        iconId="fr-icon-delete-line"
-        priority="tertiary"
-      />
+      <div
+        className={cx('fr-col-12', 'fr-col-sm-6', 'fr-col-md-6', 'fr-col-lg-4')}
+      >
+        <Link
+          onClick={async (e) => {
+            e.preventDefault();
+            window.open(documentUrl);
+          }}
+          to="#"
+          className={cx('fr-link', 'fr-mt-1w')}
+        >
+          {cropFileName(document.filename, 25)}
+          <span
+            className={cx('fr-icon-eye-line', 'fr-ml-1w', 'fr-icon--sm')}
+          ></span>
+        </Link>
+        <Button
+          title="Supprimer"
+          onClick={async (e) => {
+            e.preventDefault();
+            await onRemove(documentId);
+          }}
+          iconId="fr-icon-delete-line"
+          priority="tertiary"
+          className={clsx(cx('fr-hidden-sm', 'fr-mb-3w'), 'float-right')}
+        />
+        <Input
+          label=""
+          textArea
+          nativeTextAreaProps={{
+            placeholder: 'Légende',
+            defaultValue: document.legend || '',
+            onChange: (e) => {
+              e.preventDefault();
+              updateDocument({ documentId, legend: e.target.value });
+            },
+            rows: 1
+          }}
+          className={cx('fr-mt-2w')}
+        />
+      </div>
+      <div
+        className={clsx(
+          cx('fr-col-sm-3', 'fr-col-md-3', 'fr-col-lg-5', 'fr-mt-5w'),
+          'd-sm-none'
+        )}
+      >
+        <Button
+          title="Supprimer"
+          onClick={async (e) => {
+            e.preventDefault();
+            await onRemove(documentId);
+          }}
+          iconId="fr-icon-delete-line"
+          priority="tertiary"
+          className="float-right"
+        />
+      </div>
     </div>
   );
 };
