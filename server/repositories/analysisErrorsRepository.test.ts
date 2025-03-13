3e33c16e58eafbc2c021ededc6593a101c0b27b9
vmaubert <github@vcmb.dev>
2025-03-13 15:12:29 +0100

    ajoute la correction des analyses en bdd

---
+++ server/repositories/analysisErrorsRepository.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from 'vitest';
+import { kysely } from './kysely';
+import { analysisErrorsRepository } from './analysisErrorsRepository';
+
+import { v4 as uuidv4 } from 'uuid';
+import { Sample13Fixture } from 'maestro-shared/test/sampleFixtures';
+test("peut ajouter une erreur d'analyse en base", async () => {
+
+
+  const analysisId = uuidv4();
+  await kysely.insertInto('analysis').values([
+    {
+     sampleId: Sample13Fixture.id,
+     id: analysisId,
+     status: 'InReview'
+    }
+  ]).execute()
+
+  let analysisErrorsInDb = await kysely.selectFrom('analysisErrors').selectAll().execute()
+  expect(analysisErrorsInDb).toHaveLength(0)
+  await analysisErrorsRepository.insert(
+      analysisId,
+       [
+        {
+          reference: 'RF-00000024-PAR',
+          analysisId,
+          residueNumber: 0,
+          analysisMethod: 'Mono',
+          kind: 'Simple'
+        }
+      ], []
+  );
+  analysisErrorsInDb = await kysely.selectFrom('analysisErrors').selectAll().execute()
+  expect(analysisErrorsInDb).toHaveLength(1)
+  expect(analysisErrorsInDb[0].residues.old[0].reference).toEqual('RF-00000024-PAR')
+});
