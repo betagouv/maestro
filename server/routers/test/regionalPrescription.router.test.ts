3e89d6545b316252949eea4f7df0d78e756c132e
jrivals <jerome.rivals@gmail.com>
2025-03-24 08:52:38 +0100

    Fix test

--- server/routers/test/regionalPrescription.router.test.ts
+++ server/routers/test/regionalPrescription.router.test.ts
@@ -25,7 +25,7 @@ import {
   genRegionalPrescription
 } from 'maestro-shared/test/prescriptionFixtures';
 import { genProgrammingPlan } from 'maestro-shared/test/programmingPlanFixtures';
-import { genCreatedSample } from 'maestro-shared/test/sampleFixtures';
+import { genCreatedPartialSample } from 'maestro-shared/test/sampleFixtures';
 import { oneOf } from 'maestro-shared/test/testFixtures';
 import {
   AdminFixture,
@@ -158,14 +158,17 @@ describe('Regional prescriptions router', () => {
     createdBy: NationalCoordinator.id,
     createdAt: new Date()
   };
-  const sample = genCreatedSample({
+  const sample = genCreatedPartialSample({
     programmingPlanId: programmingPlanClosed.id,
     prescriptionId: closedControlPrescription.id,
     region: Sampler1Fixture.region as Region,
     company: CompanyFixture,
     sampler: Sampler1Fixture,
     laboratoryId: LaboratoryFixture.id,
-    status: 'Sent'
+    status: 'Sent',
+    specificData: {
+      programmingPlanKind: 'PPV'
+    }
   });
 
   beforeAll(async () => {
