97b87d12efc012a17e9ac6ffe643196a0c1ca61d
jrivals <jerome.rivals@gmail.com>
2025-03-31 15:55:06 +0200

    Fix linting

--- server/database/migrations/033-user-role.ts
+++ server/database/migrations/033-user-role.ts
@@ -6,8 +6,6 @@ export const up = async (knex: Knex) => {
 
   await knex('users').update({ role: knex.raw(`roles[1]`) });
 
-  const users = await knex('users').select('id', 'roles', 'role');
-
   await knex.schema.alterTable('users', (table) => {
     table.dropColumn('roles');
     table.string('role').notNullable().alter();
