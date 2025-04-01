d63f2bc65f824236be2155e5fc4b4e6350c67e8d
jrivals <jerome.rivals@gmail.com>
2025-04-01 16:57:30 +0200

    Génération de l'étiquette du prélèvement

--- server/services/pdfService/pdfService.ts
+++ server/services/pdfService/pdfService.ts
@@ -56,6 +56,10 @@ const generatePDF = async (template: Template, data: unknown) => {
     return a || b;
   });
 
+  handlebars.registerHelper('and', function (a, b) {
+    return a && b;
+  });
+
   const compiledTemplate = handlebars.compile(templateContent(template));
   const htmlContent = compiledTemplate(data);
 
@@ -132,7 +136,7 @@ const generateSampleSupportPDF = async (
   sample: Sample,
   sampleItems: PartialSampleItem[],
   itemNumber: number,
-  fullVersion: boolean = false
+  fullVersion: boolean
 ) => {
   const programmingPlan = await programmingPlanRepository.findUnique(
     sample.programmingPlanId as string
@@ -175,12 +179,6 @@ const generateSampleSupportPDF = async (
         quantityUnit: sampleItem?.quantityUnit
           ? QuantityUnitLabels[sampleItem.quantityUnit]
           : '',
-        compliance200263:
-          sampleItem.compliance200263 === true
-            ? 'Respectée'
-            : sampleItem.compliance200263 === false
-              ? 'Non respectée'
-              : undefined,
         currentItem: sampleItem.itemNumber === itemNumber
       })
     ),
