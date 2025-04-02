4cd71655806c934104843de43b1446b81ba06187
vmaubert <github@vcmb.dev>
2025-04-02 14:17:42 +0200

    disable cache

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -15,5 +15,5 @@ dbclient-fetcher pgsql
 pg_dump ${SCALINGO_POSTGRESQL_URL} --clean --if-exists --format=d --no-owner --no-privileges --file=./backups/
 
 restic self-update
-AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY} restic -o s3.region="${S3_REGION}" -r s3:${S3_ENDPOINT}/${S3_BACKUP_BUCKET} backup backups
-AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY} restic -o s3.region="${S3_REGION}" -r s3:${S3_ENDPOINT}/${S3_BACKUP_BUCKET} forget --keep-last 15 --keep-weekly 12 --keep-monthly 12 --keep-yearly 10 --prune
+AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY} restic -o s3.region="${S3_REGION}" -r s3:${S3_ENDPOINT}/${S3_BACKUP_BUCKET} --no-cache backup backups
+AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY} restic -o s3.region="${S3_REGION}" -r s3:${S3_ENDPOINT}/${S3_BACKUP_BUCKET} --no-cache forget --keep-last 15 --keep-weekly 12 --keep-monthly 12 --keep-yearly 10 --prune
