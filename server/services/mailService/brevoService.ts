2005c09e57500a7a5cf70c2e8764fd482ea3a6f5
vmaubert <github@vcmb.dev>
2025-04-15 13:35:50 +0200

    Revert "supprime brevo"
    This reverts commit a868f62864bb261419f486de587494cf7cd96599.

--- server/services/mailService/brevoService.ts
+++ server/services/mailService/brevoService.ts
@@ -1,29 +1,30 @@
+import * as Brevo from '@getbrevo/brevo';
 import config from '../../utils/config';
-import { MailService, SendOptions, TemplateName } from './mailService';
+import { MailService, SendOptions, TemplateName, Templates } from './mailService';
 
 class BrevoService implements MailService {
-  // private emailsApi: Brevo.TransactionalEmailsApi;
+  private emailsApi: Brevo.TransactionalEmailsApi;
 
   constructor() {
     if (!config.mailer.apiKey) {
       throw new Error('Provide an API key for Brevo');
     }
 
-    // this.emailsApi = new Brevo.TransactionalEmailsApi();
-    // this.emailsApi.setApiKey(
-    //   Brevo.TransactionalEmailsApiApiKeys.apiKey,
-    //   config.mailer.apiKey
-    // );
+    this.emailsApi = new Brevo.TransactionalEmailsApi();
+    this.emailsApi.setApiKey(
+      Brevo.TransactionalEmailsApiApiKeys.apiKey,
+      config.mailer.apiKey
+    );
   }
 
-  async send<T extends TemplateName>(_options: SendOptions<T>): Promise<void> {
-    // await this.emailsApi.sendTransacEmail({
-    //   ...options,
-    //   templateId: Templates[options.templateName].id,
-    //   to: options.recipients.map((recipient) => ({
-    //     email: recipient
-    //   }))
-    // });
+  async send<T extends TemplateName>(options: SendOptions<T>): Promise<void> {
+    await this.emailsApi.sendTransacEmail({
+      ...options,
+      templateId: Templates[options.templateName].id,
+      to: options.recipients.map((recipient) => ({
+        email: recipient
+      }))
+    });
   }
 
 }
