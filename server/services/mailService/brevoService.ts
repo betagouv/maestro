99d9b3a4ea734f372b5a3296e5039a9ae4f21596
vmaubert <github@vcmb.dev>
2025-04-17 16:35:47 +0200

    build(npm): supprime la dépendance à Brevo

--- server/services/mailService/brevoService.ts
+++ server/services/mailService/brevoService.ts
@@ -1,32 +1,47 @@
-import * as Brevo from '@getbrevo/brevo';
+import SendEmailError from 'maestro-shared/errors/sendEmailError';
 import config from '../../utils/config';
-import { MailService, SendOptions, TemplateName, Templates } from './mailService';
+import {
+  MailService,
+  SendOptions,
+  TemplateName,
+  Templates
+} from './mailService';
 
 class BrevoService implements MailService {
-  private emailsApi: Brevo.TransactionalEmailsApi;
+  apiKey: string;
 
   constructor() {
     if (!config.mailer.apiKey) {
       throw new Error('Provide an API key for Brevo');
     }
-
-    this.emailsApi = new Brevo.TransactionalEmailsApi();
-    this.emailsApi.setApiKey(
-      Brevo.TransactionalEmailsApiApiKeys.apiKey,
-      config.mailer.apiKey
-    );
+    this.apiKey = config.mailer.apiKey;
   }
 
   async send<T extends TemplateName>(options: SendOptions<T>): Promise<void> {
-    await this.emailsApi.sendTransacEmail({
+    const brevoUrl = 'https://api.brevo.com/v3/smtp/email';
+    const body = {
       ...options,
       templateId: Templates[options.templateName].id,
       to: options.recipients.map((recipient) => ({
         email: recipient
       }))
-    });
-  }
+    };
 
+    const response = await fetch(brevoUrl, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'api-key': this.apiKey,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+      console.error(response.statusText)
+      throw new SendEmailError();
+    }
+  }
 }
 
 export default function createNodemailerService(): MailService {
