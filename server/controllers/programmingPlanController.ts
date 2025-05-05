7d7f745819fe406f3b4ed0905c69d66efe5b1011
jrivals <jerome.rivals@gmail.com>
2025-05-05 14:50:47 +0200

    Mise à jour du template Brevo

--- server/controllers/programmingPlanController.ts
+++ server/controllers/programmingPlanController.ts
@@ -3,7 +3,7 @@ import { AuthenticatedRequest, ProgrammingPlanRequest } from 'express-jwt';
 import { constants } from 'http2';
 import { intersection } from 'lodash-es';
 import ProgrammingPlanMissingError from 'maestro-shared/errors/programmingPlanMissingError';
-import { RegionList } from 'maestro-shared/referential/Region';
+import { RegionList, Regions } from 'maestro-shared/referential/Region';
 import { NotificationCategoryMessages } from 'maestro-shared/schema/Notification/NotificationCategory';
 import { ContextList } from 'maestro-shared/schema/ProgrammingPlan/Context';
 import { FindProgrammingPlanOptions } from 'maestro-shared/schema/ProgrammingPlan/FindProgrammingPlanOptions';
@@ -248,7 +248,7 @@ const updateRegionalStatus = async (request: Request, response: Response) => {
             },
             nationalCoordinators,
             {
-              region: programmingPlanRegionalStatus.region
+              region: Regions[programmingPlanRegionalStatus.region].name
             }
           );
         } else {
