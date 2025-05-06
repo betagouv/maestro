196eb8583c4ece691d940601a3a98c3062bbca98
jrivals <jerome.rivals@gmail.com>
2025-05-06 17:13:12 +0200

    Nouvel onglet phase de consultation d'un plan de programmation

--- frontend/src/store/reducers/prescriptionsSlice.tsx
+++ frontend/src/store/reducers/prescriptionsSlice.tsx
@@ -1,15 +1,54 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { MatrixKind } from 'maestro-shared/referential/Matrix/MatrixKind';
 import { Region } from 'maestro-shared/referential/Region';
+import { Prescription } from 'maestro-shared/schema/Prescription/Prescription';
 import { Context } from 'maestro-shared/schema/ProgrammingPlan/Context';
-import { RegionalPrescription } from 'maestro-shared/schema/RegionalPrescription/RegionalPrescription';
+import { RegionalPrescriptionComment } from 'maestro-shared/schema/RegionalPrescription/RegionalPrescriptionComment';
 import { PrescriptionListDisplay } from 'src/views/ProgrammingPlanView/ProgrammingPlanPrescriptionList/ProgrammingPlanPrescriptionList';
+import { z } from 'zod';
 
-type PrescriptionCommentsData = {
-  matrixKind: MatrixKind;
-  regionalPrescriptions: RegionalPrescription[];
-  currentRegion?: Region;
-};
+const PrescriptionCommentsData = z.discriminatedUnion('viewBy', [
+  z.object({
+    viewBy: z.literal('MatrixKind'),
+    prescription: Prescription,
+    currentRegion: Region.nullish(),
+    regionalComments: z.array(
+      z.object({
+        region: Region,
+        comments: z
+          .array(
+            RegionalPrescriptionComment.pick({
+              comment: true,
+              createdAt: true,
+              createdBy: true
+            })
+          )
+          .min(1)
+      })
+    )
+  }),
+  z.object({
+    viewBy: z.literal('Region'),
+    region: Region,
+    currentMatrixKind: MatrixKind.nullish(),
+    matrixKindsComments: z.array(
+      z.object({
+        matrixKind: MatrixKind,
+        comments: z
+          .array(
+            RegionalPrescriptionComment.pick({
+              comment: true,
+              createdAt: true,
+              createdBy: true
+            })
+          )
+          .min(1)
+      })
+    )
+  })
+]);
+
+type PrescriptionCommentsData = z.infer<typeof PrescriptionCommentsData>;
 
 type PrescriptionsState = {
   prescriptionListContext: Context;
