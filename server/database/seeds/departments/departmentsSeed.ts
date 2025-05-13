0179ac2c5708cff7f7427844e161ff40d59899be
vmaubert <github@vcmb.dev>
2025-05-13 14:34:07 +0200

    ajoute un bout de code pour trouver des pbs

--- server/database/seeds/departments/departmentsSeed.ts
+++ server/database/seeds/departments/departmentsSeed.ts
@@ -1,7 +1,10 @@
 import { sql } from 'kysely';
+import {
+  Department,
+  DepartmentList
+} from 'maestro-shared/referential/Department';
 import { z } from 'zod';
 import { kysely } from '../../../repositories/kysely';
-import { Department, DepartmentList } from 'maestro-shared/referential/Department';
 
 const geojsonValidator = z.object({
   features: z.array(
@@ -15,14 +18,15 @@ const geojsonValidator = z.object({
 });
 
 export const departmentsSeed = async () => {
-
   const query = await fetch(
     'https://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/departements-5m.geojson'
   );
   const result = geojsonValidator.parse(await query.json());
 
   await kysely.deleteFrom('departments').execute();
-  for (const feature of result.features.filter(f => DepartmentList.includes(f.properties.code as Department))) {
+  for (const feature of result.features.filter((f) =>
+    DepartmentList.includes(f.properties.code as Department)
+  )) {
     const postgisGeometry = await sql<{
       result: unknown;
     }>`select ST_MakeValid(ST_MULTI(ST_SetSRID(ST_GeomFromGeoJSON('${sql.raw(JSON.stringify(feature.geometry))}'), 4326))) as result`.execute(
@@ -38,4 +42,23 @@ export const departmentsSeed = async () => {
       ])
       .execute();
   }
+
+  // const samples = await kysely
+  //   .selectFrom('samples')
+  //   .innerJoin('programmingPlans', 'samples.programmingPlanId', 'programmingPlans.id')
+  //   .select(['samples.id', 'samples.department', 'samples.geolocation', 'programmingPlans.year'])
+  //   .execute();
+  //
+  //
+  // console.error(['URL', 'Département déduit de la société', 'Département déduit des coordonnées GPS'].join(' '));
+  // for (const sample of samples) {
+  //   const departementBdd = await departmentRepository.getDepartment(
+  //     sample.geolocation?.x,
+  //     sample.geolocation?.y
+  //   );
+  //
+  //   if (departementBdd !== sample.department && sample.geolocation) {
+  //     console.error(`https://app.maestro.beta.gouv.fr/prelevements/${sample.year}/${sample.id}`, sample.department, sample.department ? DepartmentLabels[sample.department as Department] : '', departementBdd, departementBdd ? DepartmentLabels[sample.department as Department] : '', `"https://cartes.app/?style=winter&clic=${sample.geolocation.x}|${sample.geolocation.y}#8.85/${sample.geolocation.x}/${sample.geolocation.y}"`);
+  //   }
+  // }
 };
