83d8f7a46fc5e7585521e90ac177143b7f09f0cb
vmaubert <github@vcmb.dev>
2025-05-14 15:40:20 +0200

    fix(mascarade): supprime la modale pour les comptes non administrateurs

--- frontend/src/components/Mascarade/MascaradeModal.tsx
+++ frontend/src/components/Mascarade/MascaradeModal.tsx
@@ -1,57 +1,61 @@
-import { createModal } from '@codegouvfr/react-dsfr/Modal';
-import React, { FunctionComponent, useContext, useState } from 'react';
 import Alert from '@codegouvfr/react-dsfr/Alert';
+import { cx } from '@codegouvfr/react-dsfr/fr/cx';
+import { createModal } from '@codegouvfr/react-dsfr/Modal';
 import { Brand } from 'maestro-shared/constants';
+import React, { FunctionComponent, useContext, useState } from 'react';
 import { assert, type Equals } from 'tsafe';
-import { MascaradeContext } from './MascaradeContext';
+import { useAuthentication } from '../../hooks/useAuthentication';
 import { useFindUsersQuery } from '../../services/user.service';
 import AppSearchInput from '../_app/AppSearchInput/AppSearchInput';
 import { selectOptionsFromList } from '../_app/AppSelect/AppSelectOption';
-import { cx } from '@codegouvfr/react-dsfr/fr/cx';
+import { MascaradeContext } from './MascaradeContext';
 
 interface Props {
-  modal: ReturnType<typeof createModal>
+  modal: ReturnType<typeof createModal>;
 }
 
 export const MascaradeModal = ({ modal, ..._rest }: Props) => {
-
   assert<Equals<keyof typeof _rest, never>>();
+
+  const { hasRole } = useAuthentication();
 
   const { setMascaradeUserId } = useContext(MascaradeContext);
 
-
-  const [userId, setUserId] = useState<string | null>(null)
+  const [userId, setUserId] = useState<string | null>(null);
 
   const submit = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     modal.close();
-    setMascaradeUserId(userId)
+    setMascaradeUserId(userId);
   };
 
   return (
-    <modal.Component
-      title="Mode mascarade"
-      concealingBackdrop={false}
-      topAnchor
-      buttons={[
-        {
-          children: 'Prendre sa place',
-          onClick: submit,
-          doClosesModal: false,
-          priority: 'secondary'
-        }
-      ]}
-    >
-      Vous pouvez vous connecter à {Brand} en tant qu'un autre utilisateur.
-      <Alert
-        severity="warning"
-        className={cx('fr-my-2w')}
-        small={true}
-        description="Toute modification effectuée sera automatiquement enregistrée au nom de l'utilisateur."
-      />
-
-      <UsersSearchInput setUserId={setUserId}/>
-    </modal.Component>
+    <>
+      {hasRole('Administrator') && (
+        <modal.Component
+          title="Mode mascarade"
+          concealingBackdrop={false}
+          topAnchor
+          buttons={[
+            {
+              children: 'Prendre sa place',
+              onClick: submit,
+              doClosesModal: false,
+              priority: 'secondary'
+            }
+          ]}
+        >
+          Vous pouvez vous connecter à {Brand} en tant qu'un autre utilisateur.
+          <Alert
+            severity="warning"
+            className={cx('fr-my-2w')}
+            small={true}
+            description="Toute modification effectuée sera automatiquement enregistrée au nom de l'utilisateur."
+          />
+          <UsersSearchInput setUserId={setUserId} />
+        </modal.Component>
+      )}{' '}
+    </>
   );
 };
 
@@ -82,7 +86,7 @@ const UsersSearchInput: FunctionComponent<{
               withDefault: false
             }
           )}
-          label='Utilisateur'
+          label="Utilisateur"
           value={''}
           onSelect={(value) => setUserId(value ?? null)}
         />
