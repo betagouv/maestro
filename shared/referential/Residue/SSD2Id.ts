3b93a4349561225868dbf5ee63918525769dbcf7
vmaubert <github@vcmb.dev>
2025-05-20 15:01:48 +0200

    ajoute un superRefine

--- shared/referential/Residue/SSD2Id.ts
+++ shared/referential/Residue/SSD2Id.ts
@@ -1,4 +1,20 @@
 import { z } from 'zod';
+import { SSD2Referential } from './SSD2Referential';
 
-export const SSD2Id = z.string();
+const SSD2Ids = Object.keys(SSD2Referential);
+
+export const SSD2Id = z
+  .string({
+    required_error: 'Veuillez renseigner le résidu.'
+  })
+  .superRefine((value, ctx) => {
+    if (!SSD2Ids.includes(value)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_enum_value,
+        received: value,
+        options: SSD2Ids,
+        message: 'Veuillez renseigner un identifiant valide.'
+      });
+    }
+  });
 export type SSD2Id = z.infer<typeof SSD2Id>;
