69396ecbbe36e23162d7a290eddace9656378f6a
jrivals <jerome.rivals@gmail.com>
2025-05-21 11:18:49 +0200

    Fix build

--- server/services/pdfService/pdfService.ts
+++ server/services/pdfService/pdfService.ts
@@ -11,14 +11,16 @@ import { DepartmentLabels } from 'maestro-shared/referential/Department';
 import { getLaboratoryFullname } from 'maestro-shared/referential/Laboratory';
 import { LegalContextLabels } from 'maestro-shared/referential/LegalContext';
 import { MatrixKindLabels } from 'maestro-shared/referential/Matrix/MatrixKind';
-import { MatrixLabels } from 'maestro-shared/referential/Matrix/MatrixLabels';
 import { getMatrixPartLabel } from 'maestro-shared/referential/Matrix/MatrixPart';
 import { QuantityUnitLabels } from 'maestro-shared/referential/QuantityUnit';
 import { Regions } from 'maestro-shared/referential/Region';
 import { SSD2IdLabel } from 'maestro-shared/referential/Residue/SSD2Referential';
 import { StageLabels } from 'maestro-shared/referential/Stage';
 import { ContextLabels } from 'maestro-shared/schema/ProgrammingPlan/Context';
-import { Sample } from 'maestro-shared/schema/Sample/Sample';
+import {
+  getSampleMatrixLabel,
+  Sample
+} from 'maestro-shared/schema/Sample/Sample';
 import { PartialSampleItem } from 'maestro-shared/schema/Sample/SampleItem';
 import { formatWithTz, isDefinedAndNotNull } from 'maestro-shared/utils/utils';
 import puppeteer from 'puppeteer-core';
@@ -221,7 +223,7 @@ const generateSampleSupportPDF = async (
     legalContext: LegalContextLabels[sample.legalContext],
     stage: StageLabels[sample.stage],
     matrixKind: MatrixKindLabels[sample.matrixKind],
-    matrix: MatrixLabels[sample.matrix],
+    matrix: getSampleMatrixLabel(sample),
     matrixDetails:
       sample.specificData?.programmingPlanKind === 'PPV'
         ? sample.specificData?.matrixDetails
