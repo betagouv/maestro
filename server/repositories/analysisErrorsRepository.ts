12d73b093db079245f995fd5dd2d587dd2f5e806
vmaubert <github@vcmb.dev>
2025-06-19 14:23:32 +0200

    fix(analyses): encapsule l'accès à la base dans une transaction (#272)

--- server/repositories/analysisErrorsRepository.ts
+++ server/repositories/analysisErrorsRepository.ts
@@ -1,25 +1,27 @@
 import { PartialResidue } from 'maestro-shared/schema/Analysis/Residue/Residue';
-import { kysely } from './kysely';
+import { executeTransaction } from './kysely';
 
 const upsert = async (
   analysisId: string,
   oldResidues: Omit<PartialResidue, 'analysisId'>[],
   newResidues: Omit<PartialResidue, 'analysisId'>[]
 ) => {
-  await kysely
-    .deleteFrom('analysisErrors')
-    .where('analysisId', '=', analysisId)
-    .execute();
-  await kysely
-    .insertInto('analysisErrors')
-    .values({
-      analysisId,
-      residues: {
-        old: oldResidues,
-        new: newResidues
-      }
-    })
-    .execute();
+  return executeTransaction(async (trx) => {
+    await trx
+      .deleteFrom('analysisErrors')
+      .where('analysisId', '=', analysisId)
+      .execute();
+    await trx
+      .insertInto('analysisErrors')
+      .values({
+        analysisId,
+        residues: {
+          old: oldResidues,
+          new: newResidues
+        }
+      })
+      .execute();
+  });
 };
 
 export const analysisErrorsRepository = {
