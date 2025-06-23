d7cc44fdc797d84aa26fdfb2bb10ea675dbe370e
jrivals <jerome.rivals@gmail.com>
2025-06-23 10:18:38 +0200

    Remplace test avec react testing-library par storybook

---
+++ frontend/src/views/ProgrammingPlanView/ProgrammingPlanView.stories.tsx
@@ -0,0 +1,106 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, within } from '@storybook/test';
+import { Region, RegionList } from 'maestro-shared/referential/Region';
+import {
+  genPrescription,
+  genRegionalPrescription
+} from 'maestro-shared/test/prescriptionFixtures';
+import { genProgrammingPlan } from 'maestro-shared/test/programmingPlanFixtures';
+import { genCreatedPartialSample } from 'maestro-shared/test/sampleFixtures';
+import {
+  genAuthUser,
+  NationalCoordinator,
+  Sampler1Fixture
+} from 'maestro-shared/test/userFixtures';
+import { AuthenticatedAppRoutes } from '../../AppRoutes';
+import { ApiClient } from '../../services/apiClient';
+import {
+  defaultMockApiClientConf,
+  getMockApi
+} from '../../services/mockApiClient';
+import ProgrammingPlanView from './ProgrammingPlanView';
+
+const meta = {
+  title: 'Views/ProgrammingPlanView',
+  component: ProgrammingPlanView
+} satisfies Meta<typeof ProgrammingPlanView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const programmingPlan = {
+  ...genProgrammingPlan(),
+  status: 'InProgress',
+  statusDrom: 'InProgress'
+};
+const prescription1 = genPrescription({
+  programmingPlanId: programmingPlan.id,
+  context: 'Control'
+});
+const prescription2 = genPrescription({
+  programmingPlanId: programmingPlan.id,
+  context: 'Control'
+});
+const sample = genCreatedPartialSample({
+  sampler: Sampler1Fixture,
+  programmingPlanId: programmingPlan.id,
+  context: 'Control'
+});
+
+export const ProgrammingPlanViewForNationalCoordinator: Story = {
+  parameters: {
+    preloadedState: {
+      auth: { authUser: genAuthUser(NationalCoordinator) }
+    },
+    initialEntries: [
+      `${AuthenticatedAppRoutes.SamplesByYearRoute.link(programmingPlan.year)}/?context=Control`
+    ],
+    apiClient: getMockApi<ApiClient>({
+      ...defaultMockApiClientConf,
+      useGetSampleQuery: { data: sample },
+      useFindProgrammingPlansQuery: { data: [programmingPlan] },
+      useFindPrescriptionsQuery: { data: [prescription1, prescription2] },
+      useFindRegionalPrescriptionsQuery: (region?: Region) => ({
+        data: region
+          ? [
+              genRegionalPrescription({
+                prescriptionId: prescription1.id,
+                region
+              })
+            ]
+          : RegionList.map((region) =>
+              genRegionalPrescription({
+                prescriptionId: prescription1.id,
+                region
+              })
+            )
+      })
+    })
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(
+      canvas.findByTestId('prescriptions-cards-segment')
+    ).toBeInTheDocument();
+    await expect(
+      canvas.findByTestId('prescriptions-table-segment')
+    ).toBeInTheDocument();
+
+    await userEvent.click(canvas.getByTestId('prescriptions-table-segment'));
+
+    await expect(canvas.findByTestId('prescription-table')).toBeInTheDocument();
+
+    await expect(
+      canvas.findByTestId(`matrix-${prescription1.matrixKind}`)
+    ).toBeInTheDocument();
+    await expect(
+      canvas.findByTestId(`matrix-${prescription2.matrixKind}`)
+    ).toBeInTheDocument();
+    await expect(
+      canvas.findAllByTestId(`cell-${prescription1.matrixKind}`)
+    ).toHaveLength(RegionList.length);
+
+    await expect(canvas.findByTestId('add-matrix-button')).toBeInTheDocument();
+  }
+};
