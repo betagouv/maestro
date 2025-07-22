429be9f51a5379cc025f8ffdbe285b21d470c113
vmaubert <github@vcmb.dev>
2025-07-22 15:36:16 +0200

    ajoute l'alerte sur le dashboard

---
+++ frontend/src/views/DashboardView/DashboardNotice.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { DashboardNotice } from './DashboardNotice';
+
+const meta = {
+  title: 'Views/DashboardView/DashboardNotice',
+  component: DashboardNotice
+} satisfies Meta<typeof DashboardNotice>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    className: ''
+  }
+};
---
+++ frontend/src/views/DashboardView/DashboardNotice.tsx
@@ -0,0 +1,87 @@
+import { cx } from '@codegouvfr/react-dsfr/fr/cx';
+import Quote from '@codegouvfr/react-dsfr/Quote';
+import clsx from 'clsx';
+import { Brand } from 'maestro-shared/constants';
+import { FunctionComponent } from 'react';
+import { Link } from 'react-router';
+import { assert, type Equals } from 'tsafe';
+import manon from '../../assets/manon.jpg';
+import config from '../../utils/config';
+import './DashboardNotice.scss';
+
+type Props = {
+  className: string;
+  description: string;
+};
+export const DashboardNotice: FunctionComponent<Props> = ({
+  description,
+  className,
+  ..._rest
+}) => {
+  assert<Equals<keyof typeof _rest, never>>();
+
+  return (
+    <div className={className}>
+      <div
+        className={clsx(
+          cx('fr-callout', 'fr-callout--green-emeraude', 'fr-mb-0'),
+          'white-container'
+        )}
+      >
+        <Quote
+          author={
+            <>
+              <div>
+                <img
+                  className="fr-responsive-img"
+                  alt=""
+                  src={manon}
+                  data-fr-js-ratio="true"
+                />
+              </div>
+              <div className="manon">
+                <span className={cx('fr-text--lead', 'fr-mb-0')}>Manon</span>
+                <span className={cx('fr-text--regular', 'fr-text--light')}>
+                  de l'équipe {Brand}
+                </span>
+              </div>
+            </>
+          }
+          size="xlarge"
+          accentColor="green-emeraude"
+          text={description}
+        />
+      </div>
+      <div className="links-container">
+        <div className="d-flex-align-center">
+          <span className={clsx(cx('fr-icon-question-line'), 'icon-grey')} />
+          <div>
+            <div className={cx('fr-text--bold')}>Questions fréquentes</div>
+            <Link
+              to={`${config.websiteUrl}/aides`}
+              target="_blank"
+              className={cx('fr-link', 'fr-link--sm')}
+            >
+              Consulter notre FAQ
+            </Link>
+          </div>
+        </div>
+        <div className="d-flex-align-center">
+          <span className={clsx(cx('fr-icon-sparkling-2-line'), 'icon-grey')} />
+          <div>
+            <div className={cx('fr-text--bold')}>
+              Quoi de neuf sur Maestro ?
+            </div>
+            <Link
+              to={`${config.websiteUrl}/aides`} //TODO
+              target="_blank"
+              className={cx('fr-link', 'fr-link--sm')}
+            >
+              Consulter les nouveautés
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
