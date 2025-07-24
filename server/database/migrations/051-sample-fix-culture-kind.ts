22ba4d026bce178186da67b306b1d75fc30540c8
vmaubert <github@vcmb.dev>
2025-07-24 14:53:46 +0200

    fix(prélèvement): renseigne la méthode de culture sur les anciens prélèvements

---
+++ server/database/migrations/051-sample-fix-culture-kind.ts
@@ -0,0 +1,38 @@
+import { Knex } from 'knex';
+import { SampleMatrixSpecificData } from 'maestro-shared/schema/Sample/Sample';
+import {
+  RealizedStatusList,
+  SampleStatus
+} from 'maestro-shared/schema/Sample/SampleStatus';
+
+export const up = async (knex: Knex) => {
+  const result: {
+    rows: {
+      id: string;
+      status: SampleStatus;
+      specific_data: SampleMatrixSpecificData;
+    }[];
+  } = await knex.raw('select * from samples');
+
+  const samplesToFix = result.rows.filter((r) => {
+    return (
+      RealizedStatusList.includes(r.status) &&
+      r.specific_data &&
+      r.specific_data.programmingPlanKind === 'PPV' &&
+      !r.specific_data.cultureKind
+    );
+  });
+
+  for (const sample of samplesToFix) {
+    await knex.raw(
+      `
+          update samples
+          set specific_data = ?
+          where id = ?
+      `,
+      [{ ...sample.specific_data, cultureKind: 'Z0215' }, sample.id]
+    );
+  }
+};
+
+export const down = async (knex: Knex) => {};
