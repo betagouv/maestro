fdb47f4c7fc12d276ac0ff1c9dd370df4101c339
jrivals <jerome.rivals@gmail.com>
2025-08-19 11:39:00 +0200

    Fix migration down

--- server/database/migrations/052-notices-dashboard.ts
+++ server/database/migrations/052-notices-dashboard.ts
@@ -1,11 +1,15 @@
 import { Knex } from 'knex';
 
 export const up = async (knex: Knex) => {
-  await knex.raw(`insert into notices (type) values ('dashboard')`);
-  await knex.raw(`alter table notices
-    alter column description type text using description::text;`);
+  await knex('notices').insert({
+    type: 'dashboard'
+  });
+
+  await knex.schema.alterTable('notices', (table) => {
+    table.text('description').alter();
+  });
 };
 
 export const down = async (knex: Knex) => {
-  await knex.raw(`delete notices where type = 'dashboard'`);
+  await knex('notices').where({ type: 'dashboard' }).delete();
 };
