e559b79497927f3ba152504ecfb2847ca79d8a56
vmaubert <github@vcmb.dev>
2025-08-27 15:21:27 +0200

    ajoute des tests

--- server/services/ediSacha/raiValidator.test.ts
+++ server/services/ediSacha/raiValidator.test.ts
@@ -1,19 +1,20 @@
 import { XMLParser } from 'fast-xml-parser';
 import { readFileSync } from 'fs';
 import path from 'path';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { raiValidator } from './raiValidator';
 
-test('import une RAI', () => {
-  const file = path.join(import.meta.dirname, './example-rai-2.xml');
-  const content = readFileSync(file);
+test.each(['example-rai-1.xml', 'example-rai-2.xml'])(
+  'import une RAI',
+  (fileName) => {
+    const file = path.join(import.meta.dirname, `./${fileName}`);
+    const content = readFileSync(file);
 
-  const parser = new XMLParser({
-    parseTagValue: false
-  });
-  const obj = parser.parse(content);
+    const parser = new XMLParser({
+      parseTagValue: false
+    });
+    const xmlToJson = parser.parse(content);
 
-  console.log(obj);
-
-  raiValidator.parse(obj);
-});
+    expect(raiValidator.parse(xmlToJson)).toMatchSnapshot();
+  }
+);
