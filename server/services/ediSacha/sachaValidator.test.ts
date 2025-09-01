6d65c095338c3004580f4fb6a142f672f5329a1d
vmaubert <github@vcmb.dev>
2025-09-01 14:43:38 +0200

    ajoute des tests sur des acquittement

---
+++ server/services/ediSacha/sachaValidator.test.ts
@@ -0,0 +1,22 @@
+import { XMLParser } from 'fast-xml-parser';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { expect, test } from 'vitest';
+import { sachaValidator } from './sachaValidator';
+
+test.each([
+  'example-rai-1.xml',
+  'example-rai-2.xml',
+  'example-an-1.xml',
+  'example-an-2.xml'
+])(`import un fichier de l'EDI Sacha RAI %s`, (fileName) => {
+  const file = path.join(import.meta.dirname, `./${fileName}`);
+  const content = readFileSync(file);
+
+  const parser = new XMLParser({
+    parseTagValue: false
+  });
+  const xmlToJson = parser.parse(content);
+
+  expect(sachaValidator.parse(xmlToJson)).toMatchSnapshot();
+});
