9b80aed492e4e8005963901cd23813c0750aab15
vmaubert <github@vcmb.dev>
2025-09-16 15:41:26 +0200

    permet de générer des DAI

--- server/services/ediSacha/sachaToXML.test.ts
+++ server/services/ediSacha/sachaToXML.test.ts
@@ -1,8 +1,9 @@
+import { toMaestroDate } from 'maestro-shared/utils/date';
 import { expect, test } from 'vitest';
-import { generateXMLAcquitement } from './sachaToXML';
+import { generateXMLAcquitement, generateXMLDAI } from './sachaToXML';
 import { toSachaDateTime } from './sachaValidator';
 
-test(`génère un XML`, () => {
+test(`génère un XML d'acquittement`, () => {
   expect(
     generateXMLAcquitement(
       [
@@ -19,12 +20,12 @@ test(`génère un XML`, () => {
     "<?xml encoding="UTF-8" version="1.0"?>
     <AcquittementNonAcquittement schemavalidation="AcquittementNonAcquittement.xsd">
       <MessageParametres>
-        <CodeScenario></CodeScenario>
-        <VersionScenario></VersionScenario>
+        <CodeScenario>E.D.I. SIGAL/LABOS</CodeScenario>
+        <VersionScenario>1.0.1</VersionScenario>
         <TypeFichier>AN01</TypeFichier>
         <NomFichier></NomFichier>
-        <NomLogicielCreation></NomLogicielCreation>
-        <VersionLogicielCreation></VersionLogicielCreation>
+        <NomLogicielCreation>SIGAL</NomLogicielCreation>
+        <VersionLogicielCreation>4.0</VersionLogicielCreation>
       </MessageParametres>
       <Emetteur>
         <Nom>Maestro</Nom>
@@ -48,4 +49,83 @@ test(`génère un XML`, () => {
     "
   `
   );
+});
+
+test(`génère un XML de DAI`, () => {
+  expect(
+    generateXMLDAI(
+      {
+        DialogueActeurType: {
+          DialogueActeur: {
+            SigleIdentifiant: '',
+            Identifiant: '',
+            Nom: ''
+          }
+        },
+        DialogueDemandeIntervention: {
+          NumeroDAP: 0,
+          SigleContexteIntervention: '',
+          DateIntervention: toMaestroDate(new Date(12341234)),
+          DateModification: toSachaDateTime(new Date(55555555))
+        },
+        ReferenceEtablissementType: {
+          ReferenceEtablissement: {
+            SigleIdentifiant: '',
+            Identifiant: '',
+            Nom: ''
+          }
+        }
+      },
+      'LDA 72'
+    )
+  ).toMatchInlineSnapshot(`
+    "<?xml encoding="UTF-8" version="1.0"?>
+    <DemandesAnalyses schemavalidation="DemandesAnalyses.xsd">
+      <MessageParametres>
+        <CodeScenario>E.D.I. SIGAL/LABOS</CodeScenario>
+        <VersionScenario>1.0.1</VersionScenario>
+        <TypeFichier>DA01</TypeFichier>
+        <NomFichier></NomFichier>
+        <NomLogicielCreation>SIGAL</NomLogicielCreation>
+        <VersionLogicielCreation>4.0</VersionLogicielCreation>
+      </MessageParametres>
+      <Emetteur>
+        <Nom>Maestro</Nom>
+        <Sigle>Maestro</Sigle>
+        <LibellePartenaire>Maestro</LibellePartenaire>
+        <EmailPartenaire>Maestro</EmailPartenaire>
+        <Telephone>Maestro</Telephone>
+      </Emetteur>
+      <Destinataire>
+        <EmailPartenaire>LDA 72</EmailPartenaire>
+        <LibellePartenaire>LDA 72</LibellePartenaire>
+        <Nom>LDA 72</Nom>
+        <Sigle>LDA 72</Sigle>
+        <Telephone>LDA 72</Telephone>
+      </Destinataire>
+      <DemandeType>
+        <DialogueActeurType>
+          <DialogueActeur>
+            <SigleIdentifiant></SigleIdentifiant>
+            <Identifiant></Identifiant>
+            <Nom></Nom>
+          </DialogueActeur>
+        </DialogueActeurType>
+        <DialogueDemandeIntervention>
+          <NumeroDAP>0</NumeroDAP>
+          <SigleContexteIntervention></SigleContexteIntervention>
+          <DateIntervention>1970-01-01</DateIntervention>
+          <DateModification>1970-01-01T16:25:55</DateModification>
+        </DialogueDemandeIntervention>
+        <ReferenceEtablissementType>
+          <ReferenceEtablissement>
+            <SigleIdentifiant></SigleIdentifiant>
+            <Identifiant></Identifiant>
+            <Nom></Nom>
+          </ReferenceEtablissement>
+        </ReferenceEtablissementType>
+      </DemandeType>
+    </DemandesAnalyses>
+    "
+  `);
 });
--- server/services/ediSacha/sachaToXML.ts
+++ server/services/ediSacha/sachaToXML.ts
@@ -1,8 +1,13 @@
 import { XMLBuilder } from 'fast-xml-parser';
 import { Brand } from 'maestro-shared/constants';
 import { LaboratoryName } from 'maestro-shared/referential/Laboratory';
-import { z } from 'zod';
-import { Acquittement } from './sachaValidator';
+import { z, ZodObject } from 'zod';
+import {
+  Acquittement,
+  acquittementValidator,
+  DAI,
+  demandesAnalysesValidator
+} from './sachaValidator';
 
 const xml = z.string().brand('XML');
 type Xml = z.infer<typeof xml>;
@@ -12,59 +17,99 @@ export const generateXMLAcquitement = (
   messagesNonAcquittement: Acquittement['MessageNonAcquittement'],
   laboratory: LaboratoryName
 ): Xml => {
-  const acquitement: Acquittement = {
-    //FIXME
-    MessageParametres: {
-      CodeScenario: '',
-      VersionScenario: '',
-      TypeFichier: 'AN01',
-      NomFichier: '',
-      NomLogicielCreation: '',
-      VersionLogicielCreation: ''
+  return generateXML(
+    'AN',
+    {
+      MessageAcquittement: messagesAcquittement,
+      MessageNonAcquittement: messagesNonAcquittement
     },
-    //FIXME
-    Emetteur: {
-      Nom: Brand,
-      Sigle: Brand,
-      LibellePartenaire: Brand,
-      EmailPartenaire: Brand,
-      Telephone: Brand
-    },
-    //FIXME
-    Destinataire: {
-      EmailPartenaire: laboratory,
-      LibellePartenaire: laboratory,
-      Nom: laboratory,
-      Sigle: laboratory,
-      Telephone: laboratory
-    },
-    MessageAcquittement: messagesAcquittement,
-    MessageNonAcquittement: messagesNonAcquittement
-  };
-
-  return generateXML({
-    AcquittementNonAcquittement: {
-      ...acquitement,
-      '@_schemavalidation': 'AcquittementNonAcquittement.xsd'
-    }
-  });
+    laboratory
+  );
 };
 
-const generateXML = (content: {
-  [key: string]: {
-    '@_schemavalidation': string;
-  };
-}): Xml => {
+export const generateXMLDAI = (
+  dai: DAI['DemandeType'],
+  laboratory: LaboratoryName
+): Xml => {
+  return generateXML(
+    'DA',
+    {
+      DemandeType: dai
+    },
+    laboratory
+  );
+};
+
+type FileType = 'AN' | 'DA';
+const fileTypeConf = {
+  AN: {
+    name: 'AcquittementNonAcquittement',
+    content: acquittementValidator.omit({
+      MessageParametres: true,
+      Emetteur: true,
+      Destinataire: true
+    })
+  },
+  DA: {
+    name: 'DemandesAnalyses',
+    content: demandesAnalysesValidator.omit({
+      MessageParametres: true,
+      Emetteur: true,
+      Destinataire: true
+    })
+  }
+} as const satisfies Record<
+  FileType,
+  {
+    name: string;
+    content: ZodObject;
+  }
+>;
+
+const generateXML = <T extends FileType>(
+  fileType: T,
+  content: z.infer<(typeof fileTypeConf)[T]['content']>,
+  laboratory: LaboratoryName
+): Xml => {
   const builder = new XMLBuilder({
     ignoreAttributes: false,
     format: true
   });
+
+  const conf = fileTypeConf[fileType];
   const xmlContent = builder.build({
     '?xml': {
       '@_encoding': 'UTF-8',
       '@_version': '1.0'
     },
-    ...content
+    [conf.name]: {
+      '@_schemavalidation': `${conf.name}.xsd`,
+      //FIXME
+      MessageParametres: {
+        CodeScenario: 'E.D.I. SIGAL/LABOS',
+        VersionScenario: '1.0.1',
+        TypeFichier: `${fileType}01`,
+        NomFichier: '',
+        NomLogicielCreation: 'SIGAL',
+        VersionLogicielCreation: '4.0'
+      },
+      Emetteur: {
+        Nom: Brand,
+        Sigle: Brand,
+        LibellePartenaire: Brand,
+        EmailPartenaire: Brand,
+        Telephone: Brand
+      },
+      //FIXME
+      Destinataire: {
+        EmailPartenaire: laboratory,
+        LibellePartenaire: laboratory,
+        Nom: laboratory,
+        Sigle: laboratory,
+        Telephone: laboratory
+      },
+      ...content
+    }
   });
 
   return xml.parse(xmlContent);
