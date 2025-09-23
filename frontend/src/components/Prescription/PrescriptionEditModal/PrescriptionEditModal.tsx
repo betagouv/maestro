aaf00f2a5b35458e6aa8a27ac2abf1d86252f9bf
jrivals <jerome.rivals@gmail.com>
2025-09-23 17:44:05 +0200

    Refonte page programmation (WIP)

--- frontend/src/components/Prescription/PrescriptionEditModal/PrescriptionEditModal.tsx
+++ frontend/src/components/Prescription/PrescriptionEditModal/PrescriptionEditModal.tsx
@@ -1,19 +1,18 @@
 import { createModal } from '@codegouvfr/react-dsfr/Modal';
-import { useIsModalOpen } from '@codegouvfr/react-dsfr/Modal/useIsModalOpen';
 import Tabs from '@codegouvfr/react-dsfr/Tabs';
 import { MatrixKindLabels } from 'maestro-shared/referential/Matrix/MatrixKind';
 import { Prescription } from 'maestro-shared/schema/Prescription/Prescription';
 import { PrescriptionSubstance } from 'maestro-shared/schema/Prescription/PrescriptionSubstance';
 import { useEffect, useMemo, useRef } from 'react';
 import { useAppDispatch, useAppSelector } from 'src/hooks/useStore';
+import { pluralize } from 'src/utils/stringUtils';
 import prescriptionsSlice from '../../../store/reducers/prescriptionsSlice';
-import { pluralize } from '../../../utils/stringUtils';
 import PrescriptionBreadcrumb from '../PrescriptionBreadcrumb/PrescriptionBreadcrumb';
+import PrescriptionEditSubstances from '../PrescriptionEditSubstances/PrescriptionEditSubstances';
 import PrescriptionNotes from '../PrescriptionNotes/PrescriptionNotes';
 import PrescriptionStages from '../PrescriptionStages/PrescriptionStages';
+import PrescriptionSubstances from '../PrescriptionSubstances/PrescriptionSubstances';
 import './PrescriptionEditModal.scss';
-import PrescriptionEditSubstances from './PrescriptionEditSubstances';
-import PrescriptionEditSubstancesButtons from './PrescriptionEditSubstancesButtons';
 
 const prescriptionEditModal = createModal({
   id: `prescription-substances-modal`,
@@ -28,33 +27,11 @@ interface Props {
 }
 
 const PrescriptionEditModal = ({ onUpdatePrescriptionSubstances }: Props) => {
-  const modalRef = useRef<HTMLDivElement | null>(null);
   const dispatch = useAppDispatch();
+
   const { prescriptionEditData } = useAppSelector(
     (state) => state.prescriptions
   );
-
-  useEffect(() => {
-    const handleClickOutside = (event: any) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
-        console.log('close modal');
-        // dispatch(prescriptionsSlice.actions.setPrescriptionEditData(undefined));
-        prescriptionEditModal.close();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [modalRef]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useIsModalOpen(prescriptionEditModal, {
-    onConceal: () => {
-      console.log('close modal conceal');
-      dispatch(prescriptionsSlice.actions.setPrescriptionEditData(undefined));
-    }
-  });
 
   const title = useMemo(() => {
     if (prescriptionEditData?.mode === 'analysis') {
@@ -71,8 +48,36 @@ const PrescriptionEditModal = ({ onUpdatePrescriptionSubstances }: Props) => {
     }
   }, [prescriptionEditData]);
 
+  //Hack car la méthode "onConceal" de useIsModalOpen pose problème (ell est appelée lorsqu'on clique sur les tabs)
+  const dialogParentRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (!dialogParentRef.current) {
+      console.log('no dialogParentRef');
+      return;
+    }
+
+    const dialog = dialogParentRef.current.querySelector(
+      'dialog'
+    ) as HTMLDialogElement | null;
+    if (!dialog) {
+      console.log('no dialog');
+      return;
+    }
+
+    const mo = new MutationObserver(() => {
+      if (!dialog.open) {
+        dispatch(prescriptionsSlice.actions.setPrescriptionEditData(undefined));
+      }
+    });
+    mo.observe(dialog, { attributes: true, attributeFilter: ['open'] });
+
+    return () => {
+      mo.disconnect();
+    };
+  }, [dialogParentRef]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <div className="prescription-substances-modal">
+    <div className="prescription-substances-modal" ref={dialogParentRef}>
       <prescriptionEditModal.Component
         title={
           <>
@@ -84,7 +89,7 @@ const PrescriptionEditModal = ({ onUpdatePrescriptionSubstances }: Props) => {
         }
         topAnchor
       >
-        <div ref={modalRef}>
+        <div className="prescription-edit-modal-content">
           {prescriptionEditData?.mode === 'analysis' && (
             <PrescriptionEditSubstances
               programmingPlan={prescriptionEditData.programmingPlan!}
@@ -98,56 +103,50 @@ const PrescriptionEditModal = ({ onUpdatePrescriptionSubstances }: Props) => {
             />
           )}
           {prescriptionEditData?.mode === 'details' && (
-            <div
-              onClick={(e) => {
-                console.log('div close modal');
-                e.preventDefault();
+            <Tabs
+              tabs={[
+                {
+                  label: 'Analyses',
+                  content: (
+                    <PrescriptionSubstances
+                      {...prescriptionEditData}
+                      renderMode="inline"
+                    />
+                  )
+                },
+                {
+                  iconId:
+                    prescriptionEditData.prescription.stages.length > 0
+                      ? 'fr-icon-check-line'
+                      : undefined,
+                  label: pluralize(
+                    prescriptionEditData.prescription.stages.length
+                  )('Stade'),
+                  content: (
+                    <PrescriptionStages
+                      {...prescriptionEditData}
+                      label={`${pluralize(prescriptionEditData.prescription.stages.length)('Stade')} de prélèvement`}
+                    />
+                  )
+                },
+                {
+                  iconId:
+                    (prescriptionEditData.prescription.notes ?? '').length > 0
+                      ? 'fr-icon-quote-line'
+                      : undefined,
+                  label: 'Note',
+                  content: (
+                    <PrescriptionNotes
+                      programmingPlan={prescriptionEditData.programmingPlan}
+                      value={prescriptionEditData.prescription.notes ?? ''}
+                    />
+                  )
+                }
+              ]}
+              classes={{
+                panel: 'fr-p-3w'
               }}
-            >
-              <Tabs
-                tabs={[
-                  {
-                    label: 'Analyses',
-                    content: (
-                      <PrescriptionEditSubstancesButtons
-                        {...prescriptionEditData}
-                      />
-                    )
-                  },
-                  {
-                    iconId:
-                      prescriptionEditData.prescription.stages.length > 0
-                        ? 'fr-icon-check-line'
-                        : undefined,
-                    label: pluralize(
-                      prescriptionEditData.prescription.stages.length
-                    )('Stade'),
-                    content: (
-                      <PrescriptionStages
-                        {...prescriptionEditData}
-                        label={`${pluralize(prescriptionEditData.prescription.stages.length)('Stade')} de prélèvement`}
-                      />
-                    )
-                  },
-                  {
-                    iconId:
-                      (prescriptionEditData.prescription.notes ?? '').length > 0
-                        ? 'fr-icon-quote-line'
-                        : undefined,
-                    label: 'Note',
-                    content: (
-                      <PrescriptionNotes
-                        programmingPlan={prescriptionEditData.programmingPlan}
-                        value={prescriptionEditData.prescription.notes ?? ''}
-                      />
-                    )
-                  }
-                ]}
-                classes={{
-                  panel: 'fr-p-3w'
-                }}
-              />
-            </div>
+            />
           )}
         </div>
       </prescriptionEditModal.Component>
