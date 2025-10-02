1fe9e7fa83297f82e5d02b0c08b2d199826e53d1
jrivals <jerome.rivals@gmail.com>
2025-10-02 09:29:15 +0200

    increase maximumFileSizeToCacheInBytes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -36,10 +36,10 @@ export default defineConfig(({ mode }) => {
         strategies: 'injectManifest',
         filename: 'serviceWorker.js',
         injectManifest: {
-          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+          maximumFileSizeToCacheInBytes: 8 * 1024 * 1024
         },
         workbox: {
-          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+          maximumFileSizeToCacheInBytes: 8 * 1024 * 1024
         }
       })
     ],
