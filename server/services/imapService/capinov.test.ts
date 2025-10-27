3391ed7fe8d3099b622f47a23bd046f3c9ce0dd4
vmaubert <github@vcmb.dev>
2025-10-27 12:43:39 +0100

    fix(capinov): corrige la gestion des Non Quantifié, et  corrige une référence (#385)

--- server/services/imapService/capinov.test.ts
+++ server/services/imapService/capinov.test.ts
@@ -124,6 +124,58 @@ describe('Parse correctement le fichier CSV', () => {
   });
 });
 
+test('<LQ et "d, NQ" sont équivalent', () => {
+  const defaultLine = {
+    PREFIXE_NOM: '2025',
+    DEMANDE_NUMERO: '0003',
+    ECHANT_NUMERO: '1',
+    LOT: 'ARA-1234-333-1',
+    PARAMETRE_NOM: 'SAP00010',
+    RESULTAT_VALTEXTE: 'nd',
+    RESULTAT_VALNUM: '0',
+    PARAMETRE_LIBELLE: 'Acephate',
+    LIMITE_LQ: '0.01',
+    INCERTITUDE: '0',
+    CAS_NUMBER: '135158-54-2',
+    TECHNIQUE: 'MI MO-PC-077',
+    LMR_NUM: '0,01',
+    ECHANT_DATE_DIFFUSION: '16/04/2025',
+    COMMENTAIRE: 'Pas de problème'
+  };
+
+  const lines = [
+    { ...defaultLine, RESULTAT_VALTEXTE: 'd, NQ' },
+    { ...defaultLine, RESULTAT_VALTEXTE: '< LQ' }
+  ];
+  expect(extractAnalyzes(lines)).toMatchInlineSnapshot(`
+    [
+      {
+        "capinovRef": "2025 0003 1",
+        "notes": "Pas de problème",
+        "residues": [
+          {
+            "analysisDate": "2025-04-16",
+            "analysisMethod": "Multi",
+            "casNumber": "135158-54-2",
+            "codeSandre": null,
+            "label": "Acephate",
+            "result_kind": "NQ",
+          },
+          {
+            "analysisDate": "2025-04-16",
+            "analysisMethod": "Multi",
+            "casNumber": "135158-54-2",
+            "codeSandre": null,
+            "label": "Acephate",
+            "result_kind": "NQ",
+          },
+        ],
+        "sampleReference": "ARA-1234-333",
+      },
+    ]
+  `);
+});
+
 test('getAnalysisKeyByFileName', () => {
   expect(
     getAnalysisKeyByFileName(
