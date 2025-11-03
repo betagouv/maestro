ab0eb22401f71e3faa3e59ce7e7e858901d5ca76
vmaubert <github@vcmb.dev>
2025-11-03 09:53:31 +0100

    fix(node): corrige un test suite au passage de node 24 (#387)

--- server/services/imapService/cereco/cereco.test.ts
+++ server/services/imapService/cereco/cereco.test.ts
@@ -19,105 +19,97 @@ test('exportDataFromEmail', async () => {
     }
   ]);
 
-  expect(result).toMatchInlineSnapshot(`
-    [
+  expect(result[0]).toMatchObject({
+    notes:
+      'CONFORME vis-à-vis des paramètres analysés au règlement CE n°396/2005 du 23/02/2005 et avenants (¢).',
+    residues: [
       {
-        "notes": "CONFORME vis-à-vis des paramètres analysés au règlement CE n°396/2005 du 23/02/2005 et avenants (¢).",
-        "pdfFile": File {
-          Symbol(kHandle): Blob {},
-          Symbol(kLength): 8704,
-          Symbol(kType): "",
-        },
-        "residues": [
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "captane + THPI (en captane)",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "chlorothalonil",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "folpet + phtalimide (en folpet)",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Multi",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Prothioconazole desthio",
-            "lmr": 0.15,
-            "result": 0.038,
-            "result_kind": "Q",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Multi",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Tebuconazole",
-            "lmr": 0.5,
-            "result": 0.37,
-            "result_kind": "Q",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Acide phosphonique (somme)",
-            "lmr": 1.5,
-            "result": 0.09,
-            "result_kind": "Q",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Fosétyl-Al (exprimé en fosétyl)",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Mepiquat",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Chlormequat",
-            "result_kind": "ND",
-          },
-          {
-            "analysisDate": "2025-07-16",
-            "analysisMethod": "Mono",
-            "casNumber": null,
-            "codeSandre": null,
-            "label": "Glyphosate",
-            "result_kind": "ND",
-          },
-        ],
-        "sampleReference": "OCC-25-0001",
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'captane + THPI (en captane)',
+        result_kind: 'ND'
       },
-    ]
-  `);
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'chlorothalonil',
+        result_kind: 'ND'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'folpet + phtalimide (en folpet)',
+        result_kind: 'ND'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Multi',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Prothioconazole desthio',
+        lmr: 0.15,
+        result: 0.038,
+        result_kind: 'Q'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Multi',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Tebuconazole',
+        lmr: 0.5,
+        result: 0.37,
+        result_kind: 'Q'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Acide phosphonique (somme)',
+        lmr: 1.5,
+        result: 0.09,
+        result_kind: 'Q'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Fosétyl-Al (exprimé en fosétyl)',
+        result_kind: 'ND'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Mepiquat',
+        result_kind: 'ND'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Chlormequat',
+        result_kind: 'ND'
+      },
+      {
+        analysisDate: '2025-07-16',
+        analysisMethod: 'Mono',
+        casNumber: null,
+        codeSandre: null,
+        label: 'Glyphosate',
+        result_kind: 'ND'
+      }
+    ],
+    sampleReference: 'OCC-25-0001'
+  });
 });
