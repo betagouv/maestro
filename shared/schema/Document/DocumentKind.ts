22e3e3a14f7e5cfcbc94770bc957b50b9d45f7b3
jrivals <jerome.rivals@gmail.com>
2025-11-20 14:13:41 +0100

    Mise à jour type de documents

--- shared/schema/Document/DocumentKind.ts
+++ shared/schema/Document/DocumentKind.ts
@@ -2,13 +2,13 @@ import { z } from 'zod';
 import { CreatedSampleData } from '../Sample/Sample';
 
 export const DocumentKind = z.enum([
-  'Resource',
   'SupportDocument',
   'AnalysisReportDocument',
   'AnalysisRequestDocument',
   'SampleDocument',
-  'FicheDePlan',
-  'InstructionTechnique'
+  'ProgrammingPlanNotice',
+  'TechnicalInstruction',
+  'OtherResourceDocument'
 ]);
 
 export const getSupportDocumentFilename = (
@@ -25,22 +25,33 @@ export const getAnalysisReportDocumentFilename = (
 export const DocumentKindList: DocumentKind[] = DocumentKind.options;
 
 export const UploadDocumentKindList: DocumentKind[] = [
-  'Resource',
   'AnalysisReportDocument',
   'SampleDocument',
-  'FicheDePlan',
-  'InstructionTechnique'
+  'ProgrammingPlanNotice',
+  'TechnicalInstruction',
+  'OtherResourceDocument'
 ];
 
 export type DocumentKind = z.infer<typeof DocumentKind>;
 
 export const DocumentKindLabels: Partial<Record<DocumentKind, string>> = {
-  FicheDePlan: 'Fiche de plan',
-  InstructionTechnique: 'Instruction technique'
+  ProgrammingPlanNotice: 'Fiche de plan',
+  TechnicalInstruction: 'Instruction technique',
+  OtherResourceDocument: 'Autre'
 };
 
-export const ResourceDocumentKindList: DocumentKind[] = (
-  ['FicheDePlan', 'InstructionTechnique'] as DocumentKind[]
+export const ResourceDocumentKindList: DocumentKind[] = [
+  'ProgrammingPlanNotice',
+  'TechnicalInstruction',
+  'OtherResourceDocument'
+];
+
+export const SortedResourceDocumentKindList: DocumentKind[] = (
+  [
+    'ProgrammingPlanNotice',
+    'TechnicalInstruction',
+    'OtherResourceDocument'
+  ] as DocumentKind[]
 ).sort((a, b) => {
   return (DocumentKindLabels[a as DocumentKind] || a).localeCompare(
     DocumentKindLabels[b as DocumentKind] || b
