03c52f3ddbc0c9aded4fe7769e09b51481b40532
vmaubert <github@vcmb.dev>
2025-11-25 12:04:31 +0100

    prend en compte les modifs de Nico

--- frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisOverview/ResidueResultOverview.tsx
+++ frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisOverview/ResidueResultOverview.tsx
@@ -3,6 +3,7 @@ import Badge from '@codegouvfr/react-dsfr/Badge';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import Tag from '@codegouvfr/react-dsfr/Tag';
 import clsx from 'clsx';
+import { capitalize } from 'lodash-es';
 import { OptionalBooleanLabels } from 'maestro-shared/referential/OptionnalBoolean';
 import { isComplex } from 'maestro-shared/referential/Residue/SSD2Hierarchy';
 import { SSD2IdLabel } from 'maestro-shared/referential/Residue/SSD2Referential';
@@ -39,12 +40,6 @@ export const ResidueResultOverview: FunctionComponent<Props> = ({
   return (
     <div className={clsx('residue-detail-container')}>
       <div>
-        <h6 className={clsx('d-flex-align-center', cx('fr-m-0'))}>
-          <span>Résidu n°{residue.residueNumber}</span>
-          <Tag className={cx('fr-ml-1w', 'fr-text--regular')}>
-            {ResidueKindLabels[kind]}
-          </Tag>
-        </h6>
         {residue.compliance && (
           <div className={'d-flex-align-center'}>
             <ResidueComplianceIcon
@@ -54,7 +49,7 @@ export const ResidueResultOverview: FunctionComponent<Props> = ({
             <span
               className={clsx(
                 ResidueComplianceColor[residue.compliance],
-                cx('fr-text--xs', 'fr-m-0')
+                cx('fr-text--sm', 'fr-m-0')
               )}
             >
               {ResidueComplianceLabels[residue.compliance]}
@@ -63,112 +58,116 @@ export const ResidueResultOverview: FunctionComponent<Props> = ({
         )}
         <span>
           {residue.otherCompliance && (
-            <div className={cx('fr-text--sm', 'fr-m-0')}>
+            <div className={cx('fr-text--xs', 'fr-m-0')}>
               {quote(residue.otherCompliance)}
             </div>
           )}
         </span>
+        <h6 className={clsx('d-flex-align-center', cx('fr-m-0'))}>
+          {residue.reference
+            ? SSD2IdLabel[residue.reference]
+            : `Résidu n°${residue.residueNumber}`}
+        </h6>
+        <Badge noIcon severity="info" small={true} className={cx('fr-mt-1w')}>
+          {ResidueKindLabels[kind]}
+        </Badge>
       </div>
-      <hr className={cx('fr-my-3w')} />
-      <div className="result-container">
-        <div className="overview-header">
-          <Badge noIcon severity="info" className={cx()}>
+      <hr />
+      <div className={clsx('result-detail-bloc')}>
+        <div className="result-header">
+          <span className={cx('fr-text--lg', 'fr-text--heavy', 'fr-m-0')}>
+            Analyse
+          </span>
+          <Tag small={true} className={cx()}>
             {residue.analysisMethod
-              ? AnalysisMethodLabels[residue.analysisMethod]
+              ? capitalize(AnalysisMethodLabels[residue.analysisMethod])
               : ''}
-          </Badge>
+          </Tag>
         </div>
-        <div className={clsx('overview-content')}>
-          {kind === 'Simple' ? (
-            <ResidueValueLabel residue={residue} />
-          ) : (
-            <>
-              <span className={cx('fr-text--bold')}>
-                {residue.reference !== undefined
-                  ? SSD2IdLabel[residue.reference]
-                  : ''}
-              </span>
-              <hr />
-              {residue.analytes?.map((analyte, analyteIndex) => (
-                <div key={`analyte-${analyteIndex}`}>
-                  <div
-                    className={clsx(
-                      cx('fr-text--lg', 'fr-m-0'),
-                      'd-flex-align-center'
-                    )}
-                  >
-                    <span>
-                      {analyte.reference ? SSD2IdLabel[analyte.reference] : ''}
-                    </span>
-                    <div className="margin-left-auto"></div>
-                    {analyte.resultKind === 'Q' ? (
-                      <b>{analyte.result} mg/kg</b>
-                    ) : (
-                      <b>Détecté, non quantifié</b>
-                    )}
-                  </div>
-                </div>
-              ))}
-              <hr />
-              <h6 className={cx('fr-mb-0')}>Somme des analytes</h6>
-              <ResidueValueLabel residue={residue} />
-            </>
-          )}
-        </div>
-      </div>
-      <hr className={cx('fr-my-3w')} />
 
-      <div className={clsx('result-container')}>
-        <div className="overview-header">
-          <h6>Interprétation du résultat</h6>
-        </div>
-        <div className="overview-content">
-          <div className="d-flex-align-center">
-            Résultat brut supérieur à l'Arfd ?
-            <b className={'margin-left-auto'}>
-              {residue.resultHigherThanArfd
-                ? OptionalBooleanLabels[residue.resultHigherThanArfd]
-                : 'Non renseigné'}
-            </b>
-          </div>
-          {residue.notesOnResult && <i>{quote(residue.notesOnResult)}</i>}
-          <div className="d-flex-align-center">
-            Substance approuvée dans l'UE
-            <b className={'margin-left-auto'}>
-              {residue.substanceApproved
-                ? OptionalBooleanLabels[residue.substanceApproved]
-                : 'Non renseigné'}
-            </b>
-          </div>
-          <div className="d-flex-align-center">
-            Substance autorisée pour l'usage
-            <b className={'margin-left-auto'}>
-              {residue.substanceAuthorised
-                ? OptionalBooleanLabels[residue.substanceAuthorised]
-                : 'Non renseigné'}
-            </b>
-          </div>
-          {residue.pollutionRisk && (
-            <>
-              <div className="d-flex-align-center">
-                Pollution environnementale probable
-                <b className={'margin-left-auto'}>
-                  {OptionalBooleanLabels[residue.pollutionRisk]}
-                </b>
-              </div>
-              {residue.pollutionRisk === 'true' && (
-                <Alert
-                  severity="warning"
-                  small
-                  description="Alerte risque consommateur"
-                />
+        <ResidueValueLabel residue={residue} />
+        {residue.analytes?.length && (
+          <span
+            className={cx(
+              'fr-text--md',
+              'fr-text--heavy',
+              'fr-m-0',
+              'fr-mt-2w'
+            )}
+          >
+            Analytes
+          </span>
+        )}
+        {residue.analytes?.map((analyte, analyteIndex) => (
+          <div key={`analyte-${analyteIndex}`}>
+            <div
+              className={clsx(
+                cx('fr-text--md', 'fr-m-0'),
+                'd-flex-align-center'
               )}
-            </>
-          )}
-          {residue.notesOnPollutionRisk && (
-            <i>{quote(residue.notesOnPollutionRisk)}</i>
-          )}
+            >
+              <span>
+                {analyte.reference ? SSD2IdLabel[analyte.reference] : ''}
+              </span>
+              <div className="margin-left-auto"></div>
+              {analyte.resultKind === 'Q' ? (
+                <>{analyte.result} mg/kg</>
+              ) : (
+                <>Détecté, non quantifié</>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr />
+
+      <div className="result-detail-bloc">
+        <h6 className={cx('fr-m-0')}>Interprétation du résultat</h6>
+        <div className="d-flex-align-center">
+          Résultat brut supérieur à l'Arfd ?
+          <b className={'margin-left-auto'}>
+            {residue.resultHigherThanArfd
+              ? OptionalBooleanLabels[residue.resultHigherThanArfd]
+              : 'Non renseigné'}
+          </b>
         </div>
+        {residue.notesOnResult && <i>{quote(residue.notesOnResult)}</i>}
+        <div className="d-flex-align-center">
+          Substance approuvée dans l'UE
+          <b className={'margin-left-auto'}>
+            {residue.substanceApproved
+              ? OptionalBooleanLabels[residue.substanceApproved]
+              : 'Non renseigné'}
+          </b>
+        </div>
+        <div className="d-flex-align-center">
+          Substance autorisée pour l'usage
+          <b className={'margin-left-auto'}>
+            {residue.substanceAuthorised
+              ? OptionalBooleanLabels[residue.substanceAuthorised]
+              : 'Non renseigné'}
+          </b>
+        </div>
+        {residue.pollutionRisk && (
+          <>
+            <div className="d-flex-align-center">
+              Pollution environnementale probable
+              <b className={'margin-left-auto'}>
+                {OptionalBooleanLabels[residue.pollutionRisk]}
+              </b>
+            </div>
+            {residue.pollutionRisk === 'true' && (
+              <Alert
+                severity="warning"
+                small
+                description="Alerte risque consommateur"
+              />
+            )}
+          </>
+        )}
+        {residue.notesOnPollutionRisk && (
+          <i>{quote(residue.notesOnPollutionRisk)}</i>
+        )}
       </div>
     </div>
   );
@@ -179,11 +178,6 @@ const ResidueValueLabel = ({ residue }: Pick<Props, 'residue'>) => {
     <>
       {residue.resultKind === 'Q' && (
         <>
-          <span className={cx('fr-text--bold')}>
-            {residue.reference !== undefined
-              ? SSD2IdLabel[residue.reference]
-              : ''}
-          </span>
           <div className="d-flex-align-center">
             Valeur du résultat
             <b className={'margin-left-auto'}>{residue.result} mg/kg</b>
@@ -198,9 +192,7 @@ const ResidueValueLabel = ({ residue }: Pick<Props, 'residue'>) => {
       {residue.resultKind === 'NQ' && (
         <>
           <div className="d-flex-align-center">
-            {residue.reference !== undefined
-              ? SSD2IdLabel[residue.reference]
-              : ''}
+            Valeur du résultat
             <b className={clsx('margin-left-auto', 'align-right')}>
               Détecté, non quantifié
             </b>
