fed487bfab0780d1d7155f81dddc5b497e9d0799
vmaubert <github@vcmb.dev>
2025-11-26 18:10:02 +0100

    wip form

--- frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisForm/SampleAnalysisForm.tsx
+++ frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisForm/SampleAnalysisForm.tsx
@@ -1,24 +1,59 @@
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import clsx from 'clsx';
-import type { PartialAnalysis } from 'maestro-shared/schema/Analysis/Analysis';
+import { PartialAnalysis } from 'maestro-shared/schema/Analysis/Analysis';
 import { Sample } from 'maestro-shared/schema/Sample/Sample';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { assert, type Equals } from 'tsafe';
 import { AnalysisDocumentPreview } from '../../components/AnalysisDocumentPreview';
 import { ResidueListResult } from '../SampleAnalysisOverview/ResidueListResult';
 import { ResiduesSummary } from '../SampleAnalysisOverview/ResiduesSummary';
+import { useResiduesForm } from '../SampleDraftAnalysis/AnalysisResiduesStep/AnalysisResiduesForm';
 import { ResidueResultForm } from './ResidueResultForm';
 
 type Props = {
   sample: Sample;
-  analysis: PartialAnalysis;
+  partialAnalysis: PartialAnalysis;
 };
 export const SampleAnalysisForm: FunctionComponent<Props> = ({
   sample,
-  analysis,
+  partialAnalysis,
   ..._rest
 }) => {
   assert<Equals<keyof typeof _rest, never>>();
+
+  const [analysis, setAnalysis] = useState(
+    PartialAnalysis.parse({
+      ...partialAnalysis,
+      residues: partialAnalysis.residues ?? []
+    })
+  );
+
+  const { residues, setResidues, form, changeResidue } = useResiduesForm(
+    partialAnalysis,
+    sample
+  );
+
+  const addResidue = () =>
+    setResidues((currentResidues) => {
+      const newResidueNumber = currentResidues.length
+        ? Math.max(
+            ...currentResidues.map(({ residueNumber }) => residueNumber)
+          ) + 1
+        : 1;
+      return [
+        ...currentResidues,
+        {
+          analysisId: partialAnalysis.id,
+          residueNumber: newResidueNumber,
+          resultKind: 'Q'
+        }
+      ];
+    });
+
+  const removeResidue = (i: number) => () =>
+    setResidues((currentResidues) => {
+      return currentResidues.filter((_r, index) => i != index);
+    });
   return (
     <>
       {analysis && (
@@ -30,17 +65,22 @@ export const SampleAnalysisForm: FunctionComponent<Props> = ({
           />
 
           <h5 className={clsx('d-flex-align-center', cx('fr-m-0'))}>
-            <ResiduesSummary residues={analysis.residues ?? []} />
+            <ResiduesSummary residues={residues ?? []} />
           </h5>
 
-          {analysis.residues?.length && (
-            <ResidueListResult
-              residues={analysis.residues}
-              residuePanel={(residue) => (
-                <ResidueResultForm residue={residue} />
-              )}
-            />
-          )}
+          <ResidueListResult
+            residues={residues}
+            residuePanel={(i) => (
+              <ResidueResultForm
+                residue={residues[i]}
+                residueIndex={i}
+                form={form}
+                onDelete={removeResidue(i)}
+                onChange={changeResidue}
+              />
+            )}
+            onAddResidue={addResidue}
+          />
 
           <div>TODO</div>
         </>
