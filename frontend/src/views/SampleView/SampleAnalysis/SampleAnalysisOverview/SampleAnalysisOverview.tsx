67084010218cc0cb4ff4a07b08d8a020f0fcc62a
vmaubert <github@vcmb.dev>
2025-12-23 16:03:18 +0100

    fix(analyse): cache la conformité si celle-ci n'est pas encore saisie (#474)

--- frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisOverview/SampleAnalysisOverview.tsx
+++ frontend/src/views/SampleView/SampleAnalysis/SampleAnalysisOverview/SampleAnalysisOverview.tsx
@@ -58,41 +58,43 @@ export const SampleAnalysisOverview: FunctionComponent<Props> = ({
             />
           )}
 
-          <div
-            className={clsx(
-              cx('fr-callout'),
-              analysis.compliance
-                ? 'fr-callout--green-emeraude'
-                : 'fr-callout--pink-tuile',
-              'white-container',
-              'border-bottom'
-            )}
-          >
-            <h4 className="d-flex-align-center">
-              <div className="flex-grow-1">
-                Conformité globale de l'échantillon
-              </div>
-            </h4>
-            <div>
-              {analysis.compliance ? (
-                <h5 className={clsx('d-flex-align-center', cx('fr-mb-0'))}>
-                  <img src={check} alt="" className={cx('fr-mr-2w')} />
-                  Échantillon conforme
-                </h5>
-              ) : (
-                <h5 className={clsx('d-flex-align-center', cx('fr-mb-0'))}>
-                  <img src={close} alt="" className={cx('fr-mr-2w')} />
-                  Échantillon non conforme
-                </h5>
+          {analysis.compliance !== null && (
+            <div
+              className={clsx(
+                cx('fr-callout'),
+                analysis.compliance
+                  ? 'fr-callout--green-emeraude'
+                  : 'fr-callout--pink-tuile',
+                'white-container',
+                'border-bottom'
               )}
-              {analysis.notesOnCompliance && (
-                <div className={cx('fr-pl-9w', 'fr-text--lead')}>
-                  <div>Note additionnelle</div>
-                  <i>{quote(analysis.notesOnCompliance)}</i>
+            >
+              <h4 className="d-flex-align-center">
+                <div className="flex-grow-1">
+                  Conformité globale de l'échantillon
                 </div>
-              )}
+              </h4>
+              <div>
+                {analysis.compliance ? (
+                  <h5 className={clsx('d-flex-align-center', cx('fr-mb-0'))}>
+                    <img src={check} alt="" className={cx('fr-mr-2w')} />
+                    Échantillon conforme
+                  </h5>
+                ) : (
+                  <h5 className={clsx('d-flex-align-center', cx('fr-mb-0'))}>
+                    <img src={close} alt="" className={cx('fr-mr-2w')} />
+                    Échantillon non conforme
+                  </h5>
+                )}
+                {analysis.notesOnCompliance && (
+                  <div className={cx('fr-pl-9w', 'fr-text--lead')}>
+                    <div>Note additionnelle</div>
+                    <i>{quote(analysis.notesOnCompliance)}</i>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </>
       )}
     </>
