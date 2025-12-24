764e2ad09d0eb9bdc1af75134befb2db1e6d5f62
vmaubert <github@vcmb.dev>
2025-12-24 15:22:31 +0100

    small refactor

--- server/services/ediSacha/daoa/dai.ts
+++ server/services/ediSacha/daoa/dai.ts
@@ -1,14 +1,8 @@
 import { Sample } from 'maestro-shared/schema/Sample/Sample';
 import { toMaestroDate } from 'maestro-shared/utils/date';
-import { mkdir, readFile, writeFile } from 'node:fs/promises';
-import { tmpdir } from 'node:os';
-import path from 'path';
-import { encryptFile } from '../../gpgService';
-import { mailService } from '../../mailService';
-import { zip } from '../../zipService';
+import { sendSachaFile } from '../sachaSender';
 import {
   generateXMLDAI,
-  getZipFileName,
   loadLaboratoryAndSenderCall,
   XmlFile
 } from '../sachaToXML';
@@ -90,50 +84,7 @@ export const generateDAI = async (sample: Sample) => {
     }
 
     if (xmlFile) {
-      //--- Without PEL
-      // Create directory with xml file inside
-      const directoryPath = path.join(tmpdir(), xmlFile.fileName);
-      await mkdir(directoryPath);
-
-      const filePath = path.join(directoryPath, `${xmlFile.fileName}.xml`);
-      await writeFile(filePath, xmlFile.content);
-
-      // Zip directory
-      const zipFileName = getZipFileName(
-        xmlFile.fileType,
-        xmlFile.laboratory,
-        dateNow
-      );
-      const zipFilePath = await zip(directoryPath, zipFileName);
-
-      // Encrypt
-      const laboratoryGpgEmail = xmlFile.laboratory.sachaEmail;
-      const encryptFileName = `${zipFileName}.gpg`;
-      const encryptFilePath = await encryptFile(
-        zipFilePath,
-        laboratoryGpgEmail,
-        encryptFileName
-      );
-      const encryptFileBuffer = await readFile(encryptFilePath);
-
-      // Send by email
-      await mailService.send({
-        templateName: 'GenericTemplate',
-        attachment: [
-          {
-            name: encryptFileName,
-            content: Buffer.from(encryptFileBuffer).toString('base64')
-          }
-        ],
-        recipients: [laboratoryGpgEmail],
-        params: {
-          object: zipFileName,
-          content: zipFileName
-        }
-      });
-
-      //--- With PEL
-      // Send with KAFKA
+      await sendSachaFile(xmlFile, dateNow);
     }
   }
   return null;
