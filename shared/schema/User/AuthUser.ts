edc2dca0a9211f73b4ee0ccfedb2628a3494fae3
jrivals <jerome.rivals@gmail.com>
2026-01-27 14:08:06 +0100

    Compétences analytiques des labos (WIP)

--- shared/schema/User/AuthUser.ts
+++ shared/schema/User/AuthUser.ts
@@ -34,7 +34,8 @@ export const AuthUserRefined = AuthUser.transform(({ user, userRole }) => {
       ...user,
       region: isNationalRole(userRole) ? null : user.region,
       department: isRegionalRole(userRole) ? null : user.department,
-      companies: userRole === 'Sampler' ? user.companies : []
+      companies: userRole === 'Sampler' ? user.companies : [],
+      laboratoryId: userRole === 'LaboratoryUser' ? user.laboratoryId : null
     },
     userRole
   };
