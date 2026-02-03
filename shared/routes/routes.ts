7778783bf1222d9ddb51934125f252d270ca4315
vmaubert <github@vcmb.dev>
2026-02-03 16:59:16 +0100

    fix build

--- shared/routes/routes.ts
+++ shared/routes/routes.ts
@@ -135,27 +135,19 @@ type MaestroRouteHasProtectedMethod<T> = T extends MaestroRoutes
     : true
   : false;
 
-type FilterProtectedRoutes<R> =
-  R extends Readonly<[infer First, ...infer Rest]>
-    ? MaestroRouteHasProtectedMethod<First> extends true
-      ? [First, ...FilterProtectedRoutes<Rest>]
-      : FilterProtectedRoutes<Rest>
-    : [];
-
-type FilterUnprotectedRoutes<R> =
-  R extends Readonly<[infer First, ...infer Rest]>
-    ? MaestroRouteHasUnprotectedMethod<First> extends true
-      ? [First, ...FilterUnprotectedRoutes<Rest>]
-      : FilterUnprotectedRoutes<Rest>
-    : [];
-
 export type MaestroRoutes = (typeof MaestroRoutes)[number];
-export type ProtectedRoutes = FilterProtectedRoutes<
-  typeof MaestroRoutes
->[number];
-export type UnprotectedRoutes = FilterUnprotectedRoutes<
-  typeof MaestroRoutes
->[number];
+
+export type ProtectedRoutes = {
+  [K in MaestroRoutes]: MaestroRouteHasProtectedMethod<K> extends true
+    ? K
+    : never;
+}[MaestroRoutes];
+
+export type UnprotectedRoutes = {
+  [K in MaestroRoutes]: MaestroRouteHasUnprotectedMethod<K> extends true
+    ? K
+    : never;
+}[MaestroRoutes];
 
 export type ToRoute = {
   query?: ZodObject;
