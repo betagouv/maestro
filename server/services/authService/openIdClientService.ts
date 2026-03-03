2708a5d7bb1effa7644b86a5b8753b9839181b16
vmaubert <github@vcmb.dev>
2026-03-03 16:32:52 +0100

    chore(npm): met à jour openid-client (#598)

--- server/services/authService/openIdClientService.ts
+++ server/services/authService/openIdClientService.ts
@@ -1,7 +1,17 @@
 import { decode } from 'jsonwebtoken';
 import { AppRouteLinks } from 'maestro-shared/schema/AppRouteLinks/AppRouteLinks';
 import { AuthRedirectUrl } from 'maestro-shared/schema/Auth/AuthRedirectUrl';
-import { generators, Issuer } from 'openid-client';
+import {
+  authorizationCodeGrant,
+  buildAuthorizationUrl,
+  buildEndSessionUrl,
+  ClientSecretPost,
+  Configuration,
+  discovery,
+  fetchUserInfo,
+  randomNonce,
+  randomState
+} from 'openid-client';
 import config from '../../utils/config';
 import { AuthService } from './authService';
 
@@ -9,9 +19,9 @@ const loginCallbackUrl = `${config.application.host}${AppRouteLinks.LoginCallbac
 const logoutCallbackUrl = `${config.application.host}${AppRouteLinks.LogoutCallbackRoute.link}`;
 
 class OpenIdClientService implements AuthService {
-  private client: any;
+  private readonly client: Configuration;
 
-  private constructor(client: any) {
+  private constructor(client: Configuration) {
     this.client = client;
   }
 
@@ -26,32 +36,34 @@ class OpenIdClientService implements AuthService {
       );
     }
 
-    const issuer = await Issuer.discover(config.auth.providerUrl);
-
-    const client = new issuer.Client({
-      client_id: config.auth.clientId,
-      client_secret: config.auth.clientSecret,
-      redirect_uris: [loginCallbackUrl],
-      response_types: ['code'],
-      id_token_signed_response_alg: config.auth.tokenAlgorithm,
-      userinfo_signed_response_alg: config.auth.userInfoAlgorithm ?? undefined
-    });
+    const client = await discovery(
+      new URL(config.auth.providerUrl),
+      config.auth.clientId,
+      {
+        redirect_uris: [loginCallbackUrl],
+        response_types: ['code'],
+        id_token_signed_response_alg: config.auth.tokenAlgorithm,
+        userinfo_signed_response_alg: config.auth.userInfoAlgorithm ?? undefined
+      },
+      ClientSecretPost(config.auth.clientSecret)
+    );
 
     return new OpenIdClientService(client);
   };
 
   getAuthorizationUrl = (scope: string) => {
-    const nonce = generators.nonce();
-    const state = generators.state();
+    const nonce = randomNonce();
+    const state = randomState();
 
-    const authorizationUrl = this.client.authorizationUrl({
+    const authorizationUrl = buildAuthorizationUrl(this.client, {
       scope,
       state,
-      nonce
+      nonce,
+      redirect_uri: loginCallbackUrl
     });
 
     return {
-      url: authorizationUrl,
+      url: authorizationUrl.href,
       state,
       nonce
     };
@@ -60,17 +72,21 @@ class OpenIdClientService implements AuthService {
   authenticate = async (
     authRedirectUrl: AuthRedirectUrl
   ): Promise<{ idToken: string; name: string; email: string }> => {
-    const params = this.client.callbackParams(authRedirectUrl.url);
-    const tokenSet = await this.client.callback(loginCallbackUrl, params, {
-      state: authRedirectUrl.state ?? '',
-      nonce: authRedirectUrl.nonce ?? ''
-    });
+    const tokenSet = await authorizationCodeGrant(
+      this.client,
+      new URL(authRedirectUrl.url),
+      {
+        expectedState: authRedirectUrl.state ?? '',
+        expectedNonce: authRedirectUrl.nonce ?? ''
+      }
+    );
 
     if (!tokenSet.access_token) {
       throw new Error('No access token received');
     }
 
-    if (tokenSet.expires_at && Date.now() > tokenSet.expires_at * 1000) {
+    const expiresIn = tokenSet.expiresIn();
+    if (expiresIn !== undefined && expiresIn <= 0) {
       throw new Error('Token expired');
     }
 
@@ -90,8 +106,15 @@ class OpenIdClientService implements AuthService {
       throw new Error('Invalid nonce');
     }
 
-    const userInfo = await this.client.userinfo(
-      tokenSet.access_token as string
+    const subject = tokenSet.claims()?.sub;
+    if (!subject) {
+      throw new Error('No subject found in token claims');
+    }
+
+    const userInfo = await fetchUserInfo(
+      this.client,
+      tokenSet.access_token,
+      subject
     );
 
     if (!userInfo.email) {
@@ -110,16 +133,16 @@ class OpenIdClientService implements AuthService {
   };
 
   getLogoutUrl = (idToken: string) => {
-    const state = generators.state();
+    const state = randomState();
 
-    const logoutUrl = this.client.endSessionUrl({
+    const logoutUrl = buildEndSessionUrl(this.client, {
       id_token_hint: idToken,
       state,
       post_logout_redirect_uri: logoutCallbackUrl
     });
 
     return {
-      url: logoutUrl,
+      url: logoutUrl.href,
       state
     };
   };
