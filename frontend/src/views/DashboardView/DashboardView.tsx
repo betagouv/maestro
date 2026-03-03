88e82027f95d99e41e116d675e2a46e7bcf146ca
jrivals <jerome.rivals@gmail.com>
2026-03-03 12:02:17 +0100

    Permet le choix d'un plan dans le dashboard

--- frontend/src/views/DashboardView/DashboardView.tsx
+++ frontend/src/views/DashboardView/DashboardView.tsx
@@ -1,8 +1,13 @@
 import Button from '@codegouvfr/react-dsfr/Button';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
+import Select from '@codegouvfr/react-dsfr/Select';
 import clsx from 'clsx';
-import { ProgrammingPlanSort } from 'maestro-shared/schema/ProgrammingPlan/ProgrammingPlans';
-import { useContext, useMemo } from 'react';
+import { ProgrammingPlanKindList } from 'maestro-shared/schema/ProgrammingPlan/ProgrammingPlanKind';
+import {
+  ProgrammingPlanChecked,
+  ProgrammingPlanSort
+} from 'maestro-shared/schema/ProgrammingPlan/ProgrammingPlans';
+import { useContext, useEffect, useState } from 'react';
 import dashboard from 'src/assets/illustrations/dashboard.svg';
 import SectionHeader from 'src/components/SectionHeader/SectionHeader';
 import { useAuthentication } from 'src/hooks/useAuthentication';
@@ -17,27 +22,33 @@ import DashboardPrescriptions from './DashboardPrescriptions';
 const DashboardView = () => {
   useDocumentTitle('Tableau de bord');
   const apiClient = useContext(ApiClientContext);
-  const { hasUserPermission, user, hasNationalView } = useAuthentication();
+  const { hasUserPermission, user, hasNationalView, hasRole } =
+    useAuthentication();
   const { isOnline } = useOnLine();
 
   const { data: validatedProgrammingPlans } =
     apiClient.useFindProgrammingPlansQuery(
       {
-        kinds: user?.programmingPlanKinds,
+        kinds: hasRole('Administrator')
+          ? ProgrammingPlanKindList
+          : user?.programmingPlanKinds,
         status: ['Validated']
       },
       {
-        skip: !user?.programmingPlanKinds.length
+        skip: !user?.programmingPlanKinds.length && !hasRole('Administrator')
       }
     );
 
-  const currentValidatedProgrammingPlan = useMemo(
-    () =>
+  const [currentValidatedProgrammingPlan, setCurrentValidatedProgrammingPlan] =
+    useState<ProgrammingPlanChecked>();
+
+  useEffect(() => {
+    setCurrentValidatedProgrammingPlan(
       [...(validatedProgrammingPlans ?? [])]
         .sort(ProgrammingPlanSort)
-        .filter((pp) => pp.year <= new Date().getFullYear())[0],
-    [validatedProgrammingPlans]
-  );
+        .filter((pp) => pp.year <= new Date().getFullYear())[0]
+    );
+  }, [validatedProgrammingPlans]);
 
   if (!user) {
     return <></>;
@@ -50,21 +61,44 @@ const DashboardView = () => {
         subtitle="Un rapide coup d'oeil sur votre activité"
         illustration={dashboard}
         action={
-          currentValidatedProgrammingPlan &&
-          hasUserPermission('createSample') && (
-            <Button
-              size="large"
-              linkProps={{
-                to: AuthenticatedAppRoutes.NewSampleRoute.link(
-                  currentValidatedProgrammingPlan.year
-                ),
-                target: '_self'
-              }}
-              iconId="fr-icon-microscope-line"
-            >
-              Saisir un prélèvement
-            </Button>
-          )
+          <>
+            {(validatedProgrammingPlans?.length ?? 0) > 1 && (
+              <Select
+                label=""
+                nativeSelectProps={{
+                  value: currentValidatedProgrammingPlan?.id ?? '',
+                  onChange: (e) =>
+                    setCurrentValidatedProgrammingPlan(
+                      validatedProgrammingPlans?.find(
+                        (plan) => plan.id === e.target.value
+                      )
+                    )
+                }}
+                className={clsx(cx('fr-pt-4w'))}
+              >
+                {validatedProgrammingPlans?.map((plan) => (
+                  <option key={`plan-${plan.id}`} value={plan.id}>
+                    {plan.title} {plan.year}
+                  </option>
+                ))}
+              </Select>
+            )}
+            {currentValidatedProgrammingPlan &&
+              hasUserPermission('createSample') && (
+                <Button
+                  size="large"
+                  linkProps={{
+                    to: AuthenticatedAppRoutes.NewSampleRoute.link(
+                      currentValidatedProgrammingPlan.year
+                    ),
+                    target: '_self'
+                  }}
+                  iconId="fr-icon-microscope-line"
+                >
+                  Saisir un prélèvement
+                </Button>
+              )}
+          </>
         }
       />
       {isOnline && (
