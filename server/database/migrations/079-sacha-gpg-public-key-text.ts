7d8904887a3dd4c88093eea92c4ea259596a43da
vmaubert <github@vcmb.dev>
2026-03-05 14:55:29 +0100

    fix(gpg): corrige l'initialisation des clés gpg (#615)

---
+++ server/database/migrations/079-sacha-gpg-public-key-text.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex';
+
+export const up = async (knex: Knex) => {
+  await knex.schema.alterTable('laboratories', (table) => {
+    table.text('sacha_gpg_public_key').alter();
+  });
+};
+
+export const down = async (knex: Knex) => {
+  await knex.schema.alterTable('laboratories', (table) => {
+    table.string('sacha_gpg_public_key').alter();
+  });
+};
