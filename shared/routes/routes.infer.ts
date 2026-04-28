aabfc614faab5c54f5f2883d95506f5bdf509e1c
vmaubert <github@vcmb.dev>
2026-04-28 11:27:01 +0200

    Revert "fix(route): corrige les types du router (#801)"
    This reverts commit 44e6cf9e7b090f759eb315917a43ad5cdec8caeb.

--- shared/routes/routes.infer.ts
+++ shared/routes/routes.infer.ts
@@ -2,88 +2,33 @@ import type z from 'zod';
 import type { ZodType } from 'zod';
 import type { MaestroRoutes, routes } from './routes';
 
-type Side = 'client' | 'server';
+export type InferZod<T> = T extends ZodType ? z.infer<T> : never;
 
-type InferZod<T, Mode extends 'input' | 'output'> = T extends ZodType
-  ? Mode extends 'input'
-    ? z.input<T>
-    : z.output<T>
-  : never;
-
-// Pour body/query/params : le client envoie de l'input, le serveur reçoit de l'output.
-type RequestMode<S extends Side> = S extends 'client' ? 'input' : 'output';
-// Pour response : le client reçoit de l'output, le serveur retourne de l'input (parsé ensuite).
-type ResponseMode<S extends Side> = S extends 'client' ? 'output' : 'input';
-
-type RouteResponse<
+export type RouteResponse<
   P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P],
-  S extends Side
-> = (typeof routes)[P][M] extends { response: infer R }
-  ? InferZod<R, ResponseMode<S>>
-  : never;
+  M extends keyof (typeof routes)[P]
+> = (typeof routes)[P][M] extends { response: infer R } ? InferZod<R> : never;
 
-type RouteBody<
+export type RouteBody<
   P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P],
-  S extends Side
-> = (typeof routes)[P][M] extends { body: infer B }
-  ? InferZod<B, RequestMode<S>>
-  : never;
+  M extends keyof (typeof routes)[P]
+> = (typeof routes)[P][M] extends { body: infer B } ? InferZod<B> : never;
 
-type RouteQuery<
+export type RouteQuery<
   P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P],
-  S extends Side
-> = (typeof routes)[P][M] extends { query: infer Q }
-  ? InferZod<Q, RequestMode<S>>
-  : never;
+  M extends keyof (typeof routes)[P]
+> = (typeof routes)[P][M] extends { query: infer Q } ? InferZod<Q> : never;
 
-type RouteParams<
-  P extends MaestroRoutes,
-  S extends Side
-> = (typeof routes)[P] extends { params: infer Params }
+export type RouteParams<P extends MaestroRoutes> = (typeof routes)[P] extends {
+  params: infer Params;
+}
   ? Params extends undefined
     ? never
     : {
         [K in keyof Params]: Params[K] extends ZodType
-          ? InferZod<Params[K], RequestMode<S>>
+          ? z.infer<Params[K]>
           : never;
       }
   : never;
-
-export type ClientRouteResponse<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteResponse<P, M, 'client'>;
-export type ClientRouteBody<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteBody<P, M, 'client'>;
-export type ClientRouteQuery<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteQuery<P, M, 'client'>;
-export type ClientRouteParams<P extends MaestroRoutes> = RouteParams<
-  P,
-  'client'
->;
-
-export type ServerRouteResponse<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteResponse<P, M, 'server'>;
-export type ServerRouteBody<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteBody<P, M, 'server'>;
-export type ServerRouteQuery<
-  P extends MaestroRoutes,
-  M extends keyof (typeof routes)[P]
-> = RouteQuery<P, M, 'server'>;
-export type ServerRouteParams<P extends MaestroRoutes> = RouteParams<
-  P,
-  'server'
->;
 
 export type OrEmpty<T> = [T] extends [never] ? Record<never, never> : T;
