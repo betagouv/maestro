b17209996c1ed6f7d8b7e4cec36470093fc936b9
vmaubert <github@vcmb.dev>
2026-05-05 13:20:11 +0200

    chore(npm): met à jour knex-stringcase (#831)

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -2,15 +2,10 @@ import type { Knex } from 'knex';
 import knexStringcase from 'knex-stringcase';
 import config from './utils/config';
 
-const knexString =
-  typeof knexStringcase === 'function'
-    ? knexStringcase()
-    : //@ts-expect-error TS2239 https://github.com/Kequc/knex-stringcase/issues/28#issuecomment-2475324275
-      knexStringcase.default();
 const knexConfig: Knex.Config = {
   client: 'pg',
   acquireConnectionTimeout: 10000,
-  ...knexString,
+  ...knexStringcase(),
   connection: config.databaseUrl,
   migrations: {
     tableName: 'knex_migrations',
