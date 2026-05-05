a89f0c50b0209c297d2cdef4b96d3791b5fdabf7
Jérôme RIVALS <jrivals@users.noreply.github.com>
2026-05-05 08:36:23 +0200

    feat(prélèvement): autorise la duplication des prélèvements sur les environnements de tests (#842)

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   readonly VITE_SATELLITE_STYLE: string;
   readonly VITE_MATOMO_TAG_MANAGER_CONTAINER_URL: string;
   readonly VITE_SENTRY_FRONTEND_DNS: string;
+  readonly VITE_REVIEW_APP: string;
 }
 
 interface ImportMeta {
