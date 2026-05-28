7602322b6011fbe1a6c4d2e0dfb577770b4957dc
vmaubert <github@vcmb.dev>
2026-05-28 16:31:53 +0200

    feat(menu): dans la page Administration transforme les onglets en lien dans le menu

--- frontend/src/views/AdminView/LaboratoriesAdminView/LaboratoriesAdminView.tsx
+++ frontend/src/views/AdminView/LaboratoriesAdminView/LaboratoriesAdminView.tsx
@@ -2,29 +2,36 @@ import Button from '@codegouvfr/react-dsfr/Button';
 import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import Table from '@codegouvfr/react-dsfr/Table';
 import clsx from 'clsx';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { AuthenticatedAppRoutes } from 'src/AppRoutes';
 import { ApiClientContext } from 'src/services/apiClient';
 import { LaboratoryConfigForm } from './LaboratoryConfigForm';
 
 export const LaboratoriesAdminView = () => {
   const apiClient = useContext(ApiClientContext);
+  const navigate = useNavigate();
+  const { section, itemId } = useParams();
 
   const { data: laboratories = [] } = apiClient.useFindLaboratoriesQuery({});
-  const [selectedId, setSelectedId] = useState<string | null>(null);
 
   const { data: laboratoryConfig } = apiClient.useGetLaboratoryConfigQuery(
-    { laboratoryId: selectedId ?? '' },
-    { skip: selectedId === null }
+    { laboratoryId: itemId ?? '' },
+    { skip: !itemId }
   );
 
-  if (selectedId !== null) {
+  if (itemId) {
     return (
       <div className={cx('fr-p-2w')}>
         <h3 className={clsx('d-flex-align-center', cx('fr-mt-2w'))}>
           <Button
             priority="tertiary no outline"
             iconId="fr-icon-arrow-left-line"
-            onClick={() => setSelectedId(null)}
+            onClick={() =>
+              navigate(
+                AuthenticatedAppRoutes.AdminRoute.link(section as string)
+              )
+            }
             title="Retour à la liste"
           />
           Édition d'un laboratoire
@@ -53,7 +60,14 @@ export const LaboratoriesAdminView = () => {
             key={`${laboratory.id}-edit`}
             priority="tertiary"
             iconId="fr-icon-edit-line"
-            onClick={() => setSelectedId(laboratory.id)}
+            onClick={() =>
+              navigate(
+                AuthenticatedAppRoutes.AdminRoute.link(
+                  section as string,
+                  laboratory.id
+                )
+              )
+            }
           >
             Éditer
           </Button>
