69c611eab0504d336732bf5ba3d94dad1e2c668f
vmaubert <github@vcmb.dev>
2026-06-09 10:41:06 +0200

    Revert "fix(sentry): capture correctement les console.error (#1026)"
    This reverts commit 5a11e34b0b7ac9fc33573bf21317c10ac942c8a5.

--- server/sentry.mjs
+++ server/sentry.mjs
@@ -6,17 +6,16 @@ if (config.sentry.enabled && !config.sentry.dsn) {
 }
 
 if (config.sentry.enabled && config.sentry.dsn) {
-  const logLevels = ['error'];
+  const logLevel = ['error'];
 
   console.info(
-    `Initializing Sentry for log levels "${logLevels}" and config: ${config.sentry.dsn}`
+    `Initializing Sentry for log level "${logLevel}" and config: ${config.sentry.dsn}`
   );
 
   Sentry.init({
     dsn: config.sentry.dsn,
     environment:
       config.environment === 'production' ? 'production' : 'development',
-    tracesSampleRate: 1.0,
-    integrations: [Sentry.captureConsoleIntegration({ levels: logLevels })]
+    tracesSampleRate: 1.0
   });
 }
