7d37d6cc17e5036bcfb8b33fd43ae82242edbed8
jrivals <jerome.rivals@gmail.com>
2026-06-29 11:55:13 +0200

    Suppression du tooltip sur les entêtes de tableau

--- frontend/src/components/TableHeaderCell/TableHeaderCell.tsx
+++ frontend/src/components/TableHeaderCell/TableHeaderCell.tsx
@@ -7,17 +7,10 @@ interface Props {
 }
 
 const TableHeaderCell = ({ name, shortName }: Props) => {
-  const tooltipId = name.toLowerCase().replace(/\s+/g, '-');
-
   return (
     <>
       <div>
-        <span
-          className={clsx('pointer', 'no-wrap')}
-          aria-describedby={`tooltip-${tooltipId}`}
-        >
-          {shortName || name}
-        </span>
+        <span className={clsx('no-wrap')}>{shortName || name}</span>
       </div>
       <div
         className={cx('fr-text--xs', 'fr-text--light')}
@@ -29,14 +22,6 @@ const TableHeaderCell = ({ name, shortName }: Props) => {
       >
         {name}
       </div>
-      <span
-        className="fr-tooltip fr-placement"
-        id={`tooltip-${tooltipId}`}
-        role="tooltip"
-        aria-hidden="true"
-      >
-        {name}
-      </span>
     </>
   );
 };
