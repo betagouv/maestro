1c9ec348f22c0a51d644e2c351e15ff025fa003d
jrivals <jerome.rivals@gmail.com>
2026-07-27 17:28:36 +0200

    gère des changements de programmation nationale

--- shared/schema/User/UserRole.ts
+++ shared/schema/User/UserRole.ts
@@ -35,6 +35,7 @@ export const UserRoleList: UserRole[] = UserRole.options;
 
 const UserSamplerPermissionsList = [
   'viewProgrammingPlans',
+  'readProgrammingPlanApprovedByRegion',
   'readProgrammingPlanValidated',
   'readProgrammingPlanClosed',
   'readPrescriptions',
