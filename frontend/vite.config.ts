29e027b1c571815cbe6f4dc9915ff25f69cd062e
vmaubert <github@vcmb.dev>
2026-08-04 11:03:55 +0200

    build(vite): supprime tous les warnings de vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,13 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import react from '@vitejs/plugin-react';
 import { playwright } from '@vitest/browser-playwright';
 import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import RandomSeed from '../test/vitest-random-seed';
+import RandomSeed from '../test/vitest-random-seed.mts';
 
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
+const dirname = import.meta.dirname;
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
